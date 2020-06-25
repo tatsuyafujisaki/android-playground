075c708a97159a0eb99ffddaaa6cc90fc352bdda
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-06-25 13:22:29 +0900

    Update GraphicsUtil.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/GraphicsUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/GraphicsUtil.kt
@@ -1,10 +1,39 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.graphics.Matrix
+import android.os.Build
+import android.provider.MediaStore
+import androidx.core.net.toUri
+import com.bumptech.glide.Glide
 
 object GraphicsUtil {
+    fun downloadBitmap(context: Context, url: String): Bitmap? {
+        val uri = url.toUri()
+        return try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ImageDecoder.createSource(context.contentResolver, uri)
+                    .let(ImageDecoder::decodeBitmap)
+            } else {
+                MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+            }
+        } catch (_: Exception) {
+            null
+        }
+    }
+
+    fun downloadBitmap2(context: Context, url: String): Bitmap? =
+        try {
+            Glide.with(context).asBitmap().load(url).submit().get()
+        } catch (_: Exception) {
+            null
+        }
+
     fun rotateClockwise(bitmap: Bitmap, degrees: Float) =
-            Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height,
-                    Matrix().apply { postRotate(degrees) }, true)
+        Bitmap.createBitmap(
+            bitmap, 0, 0, bitmap.width, bitmap.height,
+            Matrix().apply { postRotate(degrees) }, true
+        )
 }
