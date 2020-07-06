8ec7a5892e6bb07165533ab579e727fe148fc43a
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-07-06 19:51:55 +0900

    Update StringUtil.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/StringUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/StringUtil.kt
@@ -7,4 +7,14 @@ object StringUtil {
      * Decode HTML entities such as "&amp;" to "&".
      */
     fun decode(html: String) = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT).toString()
+    
+    fun String.color(@ColorInt color: Int) =
+        SpannableString(this).apply {
+            setSpan(
+                ForegroundColorSpan(color),
+                0,
+                length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
 }
