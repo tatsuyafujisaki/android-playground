eb2718abb68b8473333e39edbbdedf61e894c1cc
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-07-16 15:46:09 +0900

    Update LiveDataUtil.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/LiveDataUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/LiveDataUtil.kt
@@ -1,63 +1,33 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.*
 
 object LiveDataUtil {
     fun <T> MutableLiveData<T>.notifyObserver() {
         value = value
     }
 
-    fun <T1, T2, R> mediate(
-        liveData1: LiveData<T1>,
-        liveData2: LiveData<T2>,
-        onChange: (T1?, T2?) -> R
-    ) = MediatorLiveData<R>().apply {
+    fun <A, B, C> combine(
+        liveData1: LiveData<A>,
+        liveData2: LiveData<B>,
+        onChanged: (A, B) -> C
+    ) =
+        liveData1.switchMap { a ->
+            liveData2.map { b ->
+                onChanged(a, b)
+            }
+        }
+
+    fun <A, B, C> mediate(
+        liveData1: LiveData<A>,
+        liveData2: LiveData<B>,
+        onChanged: (A?, B?) -> C
+    ) = MediatorLiveData<C>().apply {
         addSource(liveData1) {
-            value = onChange(liveData1.value, liveData2.value)
+            value = onChanged(liveData1.value, liveData2.value)
         }
         addSource(liveData2) {
-            value = onChange(liveData1.value, liveData2.value)
-        }
-    }
-
-    fun <T1, T2, R> mediateNoNull(
-        liveData1: LiveData<T1>,
-        liveData2: LiveData<T2>,
-        default: R,
-        onChange: (T1, T2) -> R
-    ) = MediatorLiveData<R>().apply {
-        fun onChange() = if(liveData1.value == null || liveData2.value == null) {
-            default
-        } else {
-            onChange(liveData1.value!!, liveData2.value!!)
-        }
-
-        addSource(liveData1) {
-            value = onChange()
-        }
-        addSource(liveData2) {
-            value = onChange()
-        }
-    }
-
-    fun <T1, T2, R> mediateSets(
-        liveData1: LiveData<T1>,
-        liveData2: LiveData<T2>,
-        onChange: (T1, T2) -> Set<R>
-    ) = MediatorLiveData<Set<R>>().apply {
-        fun onChange() = if(liveData1.value == null || liveData2.value == null) {
-            emptySet()
-        } else {
-            onChange(liveData1.value!!, liveData2.value!!)
-        }
-
-        addSource(liveData1) {
-            value = onChange()
-        }
-        addSource(liveData2) {
-            value = onChange()
+            value = onChanged(liveData1.value, liveData2.value)
         }
     }
 }
