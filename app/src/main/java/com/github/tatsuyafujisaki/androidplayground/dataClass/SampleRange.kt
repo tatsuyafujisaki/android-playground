bff20cb94ca5660680d6b3d14cc67008784b78d3
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-07-24 14:32:51 +0900

    Add SampleRange.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/dataClass/SampleRange.kt
@@ -0,0 +1,20 @@
+package com.github.tatsuyafujisaki.androidplayground.dataClass
+
+/**
+ * @IntRange must be fully qualified or Kotlin confuses it with [kotlin.ranges.IntRange].
+ * On a separate note, you cannot validate "lower <= upper" with @IntRange
+ * because @IntRange can take only compile-time constants.
+ */
+data class SampleRange(
+    @androidx.annotation.IntRange(from = MIN, to = MAX) val lower: Int,
+    @androidx.annotation.IntRange(from = MIN, to = MAX) val upper: Int
+) {
+    init {
+        require(lower <= upper)
+    }
+
+    companion object {
+        const val MIN = 0L
+        const val MAX = 10000L
+    }
+}
