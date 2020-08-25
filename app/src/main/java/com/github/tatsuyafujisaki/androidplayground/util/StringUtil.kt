e693d4a73c5a5fc551eefd11d20020903f744a98
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-08-25 13:07:28 +0900

    Add isValidEmailAddress(...) and isValidPhoneNumber(...)

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/StringUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/StringUtil.kt
@@ -4,14 +4,21 @@ import android.text.Html
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.util.Patterns
 import androidx.annotation.ColorInt
 
 object StringUtil {
+    fun isValidEmailAddress(emailAddress: String) =
+        Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()
+
+    fun isValidPhoneNumber(phoneNumber: String) =
+        Patterns.PHONE.matcher(phoneNumber).matches()
+
     /**
      * Decode HTML entities such as "&amp;" to "&".
      */
     fun decode(html: String) = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT).toString()
-    
+
     fun String.color(@ColorInt color: Int) =
         SpannableString(this).apply {
             setSpan(
