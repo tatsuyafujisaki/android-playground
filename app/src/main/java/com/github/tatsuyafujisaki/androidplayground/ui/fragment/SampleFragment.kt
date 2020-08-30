7a22a09fd5a20c32a5b3feeae20a2e5466d24828
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-08-30 22:21:55 +0900

    Update fragment_sample.xml

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/ui/fragment/SampleFragment.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/ui/fragment/SampleFragment.kt
@@ -4,8 +4,12 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.github.tatsuyafujisaki.androidplayground.R
 import com.github.tatsuyafujisaki.androidplayground.dataClass.SampleRepository
+import com.github.tatsuyafujisaki.androidplayground.databinding.FragmentSampleBinding
+import com.github.tatsuyafujisaki.androidplayground.util.viewBindings
 
 class SampleFragment : Fragment(R.layout.fragment_sample) {
+    private val binding by viewBindings(FragmentSampleBinding::bind)
+
     companion object {
         private const val FOO_KEY = "foo"
         private const val BAR_KEY = "bar"
