dcf66773ecfbc680a30f16bbe25601a9b4ac4e80
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-08-31 10:43:57 +0900

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/github/tatsuyafujisaki/androidplayground/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/tatsuyafujisaki/androidplayground/ExampleInstrumentedTest.kt
@@ -1,22 +1,17 @@
 package com.github.tatsuyafujisaki.androidplayground
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.ext.truth.os.ParcelableSubject.assertThat
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.github.tatsuyafujisaki.androidplayground", appContext.packageName)
+    fun testAppContext() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        assertThat("com.github.tatsuyafujisaki.androidplayground.debug")
+            .isEqualTo(context.packageName)
     }
 }
