daefce5de45d340bcc8bc93fffb7c29b83bc22d4
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-09-18 16:15:16 +0900

    Replace the Application class when using Robolectric

--- app/src/test/java/com/github/tatsuyafujisaki/androidplayground/RobolectricTest.kt
+++ app/src/test/java/com/github/tatsuyafujisaki/androidplayground/RobolectricTest.kt
@@ -1,13 +1,16 @@
 package com.github.tatsuyafujisaki.androidplayground
 
+import android.app.Application
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(application = Application::class)
 class RobolectricTest {
     private val context = ApplicationProvider.getApplicationContext<Context>()
 
