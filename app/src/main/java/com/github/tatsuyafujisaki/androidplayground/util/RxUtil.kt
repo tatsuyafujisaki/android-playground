9926ae34f5cb01cab8fcbbec83ffa536da82793f
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-10-18 22:48:33 +0900

    Create RxUtil.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/RxUtil.kt
@@ -0,0 +1,14 @@
+package com.github.tatsuyafujisaki.androidplayground.util
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.ObservableTransformer
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+object RxUtil {
+    fun <T> subscribeObserveTransformer() =
+        ObservableTransformer<T, T> { observable ->
+            observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+        }
+}
