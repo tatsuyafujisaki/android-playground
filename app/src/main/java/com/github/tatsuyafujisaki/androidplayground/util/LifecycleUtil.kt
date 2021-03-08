f36467279d7dae883f50e551f8e268d707a22650
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-03-09 00:15:04 +0900

    Add a logger to Lifecycle.onStateChanged()

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/LifecycleUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/LifecycleUtil.kt
@@ -1,5 +1,6 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
+import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleObserver
@@ -8,6 +9,8 @@ import androidx.lifecycle.OnLifecycleEvent
 object LifecycleUtil {
     fun Lifecycle.onStateChanged() {
         addObserver(LifecycleEventObserver { source, event ->
+            Log.d(source::class.java.simpleName, event.name)
+
             when (event) {
                 Lifecycle.Event.ON_CREATE -> {
                 }
