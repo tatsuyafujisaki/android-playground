ee2883182e97e0ad007cc939e4d7d888505849d8
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-01 19:14:14 +0900

    Not pass the layout ID to the constructor of a Fragment

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/ui/fragment/ExploreFragment.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/ui/fragment/ExploreFragment.kt
@@ -1,15 +1,13 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.github.tatsuyafujisaki.androidplayground.R
 import com.github.tatsuyafujisaki.androidplayground.databinding.FragmentExploreBinding
 
-class ExploreFragment : Fragment(R.layout.fragment_explore) {
+class ExploreFragment : Fragment() {
     private var _binding: FragmentExploreBinding? = null
     private val binding get() = _binding!!
 
@@ -18,14 +16,12 @@ class ExploreFragment : Fragment(R.layout.fragment_explore) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        Log.d(TAG, object {}.javaClass.enclosingMethod!!.name)
         _binding = FragmentExploreBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        Log.d(TAG, object {}.javaClass.enclosingMethod!!.name)
         _binding = null
     }
 
--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/ui/fragment/SampleFragment.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/ui/fragment/SampleFragment.kt
@@ -1,15 +1,13 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.github.tatsuyafujisaki.androidplayground.R
 import com.github.tatsuyafujisaki.androidplayground.databinding.FragmentSampleBinding
 
-class SampleFragment : Fragment(R.layout.fragment_sample) {
+class SampleFragment : Fragment() {
     private var _binding: FragmentSampleBinding? = null
     private val binding get() = _binding!!
 
@@ -18,14 +16,12 @@ class SampleFragment : Fragment(R.layout.fragment_sample) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        Log.d(TAG, object {}.javaClass.enclosingMethod!!.name)
         _binding = FragmentSampleBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        Log.d(TAG, object {}.javaClass.enclosingMethod!!.name)
         _binding = null
     }
 
