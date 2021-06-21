da15d0191bb7f4cf3543e6bebaaa1fefc34311b1
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-06-21 21:13:03 +0900

    Create ViewUtil

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/ResourceUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/ResourceUtil.kt
@@ -1,13 +1,11 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
 import android.content.res.Resources
-import android.view.View
 import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.updatePadding
 import com.github.tatsuyafujisaki.androidplayground.util.ContextUtil.getColor2
 import com.github.tatsuyafujisaki.androidplayground.util.ContextUtil.getDrawable2
 
@@ -29,35 +27,4 @@ object ResourceUtil {
     fun Resources.getDrawable2(@DrawableRes id: Int) = ResourcesCompat.getDrawable(this, id, null)
     fun Resources.getPixel(@DimenRes id: Int) = getDimensionPixelSize(id)
     fun Resources.getString2(@StringRes id: Int) = getString(id)
-
-    fun View.updatePaddingByRes(
-        resources: Resources,
-        @DimenRes leftId: Int? = null,
-        @DimenRes topId: Int? = null,
-        @DimenRes rightId: Int? = null,
-        @DimenRes bottomId: Int? = null
-    ) {
-        updatePadding(
-            leftId?.let(resources::getDimensionPixelSize) ?: paddingLeft,
-            topId?.let(resources::getDimensionPixelSize) ?: paddingTop,
-            rightId?.let(resources::getDimensionPixelSize) ?: paddingRight,
-            bottomId?.let(resources::getDimensionPixelSize) ?: paddingBottom
-        )
-    }
-
-    fun View.updatePaddingInDp(
-        leftDp: Int? = null,
-        topDp: Int? = null,
-        rightDp: Int? = null,
-        bottomDp: Int? = null
-    ) {
-        fun toPx2(dp: Int) = (dp * Resources.getSystem().displayMetrics.density).toInt()
-
-        updatePadding(
-            leftDp?.let(::toPx2) ?: paddingLeft,
-            topDp?.let(::toPx2) ?: paddingTop,
-            rightDp?.let(::toPx2) ?: paddingRight,
-            bottomDp?.let(::toPx2) ?: paddingBottom
-        )
-    }
 }
---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/ViewUtil.kt
@@ -0,0 +1,39 @@
+package com.github.tatsuyafujisaki.androidplayground.util
+
+import android.content.res.Resources
+import android.view.View
+import androidx.annotation.DimenRes
+import androidx.core.view.updatePadding
+
+object ViewUtil {
+    fun View.updatePaddingByRes(
+        resources: Resources,
+        @DimenRes leftId: Int? = null,
+        @DimenRes topId: Int? = null,
+        @DimenRes rightId: Int? = null,
+        @DimenRes bottomId: Int? = null
+    ) {
+        updatePadding(
+            leftId?.let(resources::getDimensionPixelSize) ?: paddingLeft,
+            topId?.let(resources::getDimensionPixelSize) ?: paddingTop,
+            rightId?.let(resources::getDimensionPixelSize) ?: paddingRight,
+            bottomId?.let(resources::getDimensionPixelSize) ?: paddingBottom
+        )
+    }
+
+    fun View.updatePaddingInDp(
+        leftDp: Int? = null,
+        topDp: Int? = null,
+        rightDp: Int? = null,
+        bottomDp: Int? = null
+    ) {
+        fun toPx2(dp: Int) = (dp * Resources.getSystem().displayMetrics.density).toInt()
+
+        updatePadding(
+            leftDp?.let(::toPx2) ?: paddingLeft,
+            topDp?.let(::toPx2) ?: paddingTop,
+            rightDp?.let(::toPx2) ?: paddingRight,
+            bottomDp?.let(::toPx2) ?: paddingBottom
+        )
+    }
+}
