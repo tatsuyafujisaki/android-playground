7d2d99c4c327ec6faff76879f790c7fef1476dbf
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-06-21 21:11:03 +0900

    Update ResourceUtil.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/ResourceUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/ResourceUtil.kt
@@ -1,15 +1,23 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
 import android.content.res.Resources
+import android.view.View
 import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.updatePadding
 import com.github.tatsuyafujisaki.androidplayground.util.ContextUtil.getColor2
 import com.github.tatsuyafujisaki.androidplayground.util.ContextUtil.getDrawable2
 
 object ResourceUtil {
+    /** Converts dp to pixel */
+    val Int.toPx: Int get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+    /** Converts pixel to dp */
+    val Int.toDp: Int get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+
     /**
      * Prefer [ContextUtil.getColor2] for simplicity.
      */
@@ -22,9 +30,34 @@ object ResourceUtil {
     fun Resources.getPixel(@DimenRes id: Int) = getDimensionPixelSize(id)
     fun Resources.getString2(@StringRes id: Int) = getString(id)
 
-    /** Converts dp to pixel */
-    val Int.toPx: Int get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+    fun View.updatePaddingByRes(
+        resources: Resources,
+        @DimenRes leftId: Int? = null,
+        @DimenRes topId: Int? = null,
+        @DimenRes rightId: Int? = null,
+        @DimenRes bottomId: Int? = null
+    ) {
+        updatePadding(
+            leftId?.let(resources::getDimensionPixelSize) ?: paddingLeft,
+            topId?.let(resources::getDimensionPixelSize) ?: paddingTop,
+            rightId?.let(resources::getDimensionPixelSize) ?: paddingRight,
+            bottomId?.let(resources::getDimensionPixelSize) ?: paddingBottom
+        )
+    }
 
-    /** Converts pixel to dp */
-    val Int.toDp: Int get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+    fun View.updatePaddingInDp(
+        leftDp: Int? = null,
+        topDp: Int? = null,
+        rightDp: Int? = null,
+        bottomDp: Int? = null
+    ) {
+        fun toPx2(dp: Int) = (dp * Resources.getSystem().displayMetrics.density).toInt()
+
+        updatePadding(
+            leftDp?.let(::toPx2) ?: paddingLeft,
+            topDp?.let(::toPx2) ?: paddingTop,
+            rightDp?.let(::toPx2) ?: paddingRight,
+            bottomDp?.let(::toPx2) ?: paddingBottom
+        )
+    }
 }
