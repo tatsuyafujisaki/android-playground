265ecda4c6df8ca9beba622c20f5cb13ce10319f
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-07-07 22:02:19 +0900

    Delete MainApplication.kt

--- app/src/release/java/com/github/tatsuyafujisaki/androidplayground/MainApplication.kt
+++
@@ -1,5 +0,0 @@
-package com.github.tatsuyafujisaki.androidplayground
-
-class MainApplication : dagger.android.DaggerApplication() {
-    override fun applicationInjector() = DaggerAppComponent.factory().create(this)
-}
