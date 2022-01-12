df0907e083ac26e268549b9c7f9b8071ecefbd04
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-01-12 13:33:06 +0900

    Create ColumnSample.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/ColumnSample.kt
@@ -0,0 +1,51 @@
+package com.github.tatsuyafujisaki.androidplayground.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ColumnSample(
+    height: Dp = 100.dp,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Center,
+    horizontalAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
+    background: Color = Color.White,
+    onClick: (() -> Unit)? = null,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(height)
+            .background(background)
+            .clickable(enabled = onClick != null, onClick = onClick ?: {}),
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment,
+        content = content
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        ColumnSample {
+            Text("bacon")
+            Text("lettuce")
+            Text("tomato")
+        }
+    }
+}
