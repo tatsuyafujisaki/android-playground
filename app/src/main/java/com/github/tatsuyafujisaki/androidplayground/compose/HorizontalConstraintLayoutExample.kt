5fe4c1d1cb01d8f1499e82b57a22db66c4d1c211
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-01-14 14:51:58 +0900

    Update HorizontalConstraintLayoutExample.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/HorizontalConstraintLayoutExample.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/HorizontalConstraintLayoutExample.kt
@@ -1,28 +1,31 @@
 package com.github.tatsuyafujisaki.androidplayground.compose
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 
 @Preview(showBackground = true)
 @Composable
-fun HorizontalConstraintLayoutExample(height: Dp = 100.dp) {
+fun HorizontalConstraintLayoutExample() {
     ConstraintLayout(
         Modifier
             .fillMaxWidth()
-            .height(height)
+            .height(100.dp)
     ) {
         val (text1, text2) = createRefs()
 
         Text(
             text = "Hello",
-            modifier = Modifier.constrainAs(text1) {
+            modifier = Modifier
+                .background(Color.Blue)
+                .constrainAs(text1) {
                 start.linkTo(parent.start, 8.dp)
                 top.linkTo(parent.top, 8.dp)
                 end.linkTo(text2.start, 8.dp)
@@ -31,7 +34,9 @@ fun HorizontalConstraintLayoutExample(height: Dp = 100.dp) {
         )
         Text(
             text = "World",
-            modifier = Modifier.constrainAs(text2) {
+            modifier = Modifier
+                .background(Color.Red)
+                .constrainAs(text2) {
                 start.linkTo(text1.end, 8.dp)
                 top.linkTo(parent.top, 8.dp)
                 end.linkTo(parent.end, 8.dp)
