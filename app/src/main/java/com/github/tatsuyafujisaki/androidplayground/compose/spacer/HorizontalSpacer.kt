c13b4da49855e6521920cdcd84f0696b29118318
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-03-01 21:13:04 +0900

    Add spacers

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/spacer/HorizontalSpacer.kt
@@ -0,0 +1,24 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HorizontalSpacer(height: Dp) {
+    Spacer(Modifier.height(height))
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewHorizontalSpacer() {
+    Column {
+        Text("Hello")
+        HorizontalSpacer(100.dp)
+        Text("World")
+    }
+}
---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/spacer/VerticalSpacer.kt
@@ -0,0 +1,24 @@
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun VerticalSpacer(width: Dp) {
+    Spacer(Modifier.width(width))
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewVerticalSpacer() {
+    Row {
+        Text("Hello")
+        VerticalSpacer(100.dp)
+        Text("World")
+    }
+}
