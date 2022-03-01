c0692a4c23e7c32d5cc3237089333a157bc5c7a1
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-03-01 21:19:33 +0900

    Create HorizontalDivider.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/divider/HorizontalDivider.kt
@@ -0,0 +1,32 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HorizontalDivider(
+    color: Color,
+    thickness: Dp
+) {
+    Divider(
+        color = color,
+        thickness = thickness
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewHorizontalSpacer() {
+    Column {
+        Text("Hello")
+        HorizontalDivider(
+            color = Color.Yellow,
+            thickness = 100.dp
+        )
+        Text("World")
+    }
+}
