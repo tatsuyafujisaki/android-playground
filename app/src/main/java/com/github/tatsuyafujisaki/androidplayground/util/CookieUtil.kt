8b64573a4ecf958b92e714134d1552ec67449625
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-04-20 16:20:32 +0900

    Update CookieUtil.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/CookieUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/CookieUtil.kt
@@ -3,19 +3,19 @@ package com.github.tatsuyafujisaki.androidplayground.util
 import android.webkit.CookieManager
 
 object CookieUtil {
-    fun CookieManager.getCookies(url: String): Map<String, String> =
-        getCookie(url)
-            ?.split(';')
-            ?.map {
-                val (key, value) = it.split('=')
-                key.trim() to value.trim()
-            }?.toMap()
-            .orEmpty()
+    fun getCookies(url: String): Map<String, String> {
+        val cookie = CookieManager.getInstance().getCookie(url) ?: return emptyMap()
 
-    fun CookieManager.setCookies(url: String, cookies: Map<String, String>) {
+        return cookie.split(';').associate {
+            val (key, value) = it.split('=')
+            key.trim() to value.trim()
+        }
+    }
+
+    fun setCookies(url: String, cookies: Map<String, String>) {
+        val cookieManager = CookieManager.getInstance()
         for (cookie in cookies) {
-            setCookie(url, "${cookie.key}=${cookie.value}")
+            cookieManager.setCookie(url, "${cookie.key}=${cookie.value}")
         }
     }
 }
-
