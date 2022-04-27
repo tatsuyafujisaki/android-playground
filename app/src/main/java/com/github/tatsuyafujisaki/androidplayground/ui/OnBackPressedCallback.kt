c163d93ed69811d9f75bc8c0aa9f30de8424ada3
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-04-27 17:21:23 +0900

    Create OnBackPressedCallback.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/ui/OnBackPressedCallback.kt
@@ -0,0 +1,29 @@
+package com.github.tatsuyafujisaki.androidplayground.ui
+
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+
+class OnBackPressedCallback(fragment: Fragment, callback: () -> Unit) {
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            callback()
+        }
+    }
+
+    init {
+        fragment.lifecycle.addObserver(
+            LifecycleEventObserver { _, event ->
+                when (event) {
+                    Lifecycle.Event.ON_START -> fragment
+                        .requireActivity()
+                        .onBackPressedDispatcher
+                        .addCallback(fragment.viewLifecycleOwner, onBackPressedCallback)
+                    Lifecycle.Event.ON_STOP -> onBackPressedCallback.remove()
+                    else -> {}
+                }
+            }
+        )
+    }
+}
