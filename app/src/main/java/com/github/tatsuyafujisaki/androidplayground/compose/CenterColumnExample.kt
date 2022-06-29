8e085bb67c61cd446963b8daec716e5bbc5384b4
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-06-29 10:56:13 +0900

    Add verticalScroll(rememberScrollState())

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/CenterColumnExample.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/CenterColumnExample.kt
@@ -2,7 +2,13 @@ package com.github.tatsuyafujisaki.androidplayground.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,6 +30,7 @@ fun CenterColumnExample(
             .fillMaxWidth()
             .height(height)
             .background(background)
+            .verticalScroll(rememberScrollState())
             .clickable(enabled = onClick != null, onClick = onClick ?: {}),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
