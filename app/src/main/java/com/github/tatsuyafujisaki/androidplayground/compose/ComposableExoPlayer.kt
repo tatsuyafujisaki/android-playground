f2ab929626364dbb16b4e5ebd8129cd36101656d
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-07-04 18:04:33 +0900

    Update ComposableExoPlayer.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/ComposableExoPlayer.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/ComposableExoPlayer.kt
@@ -1,23 +1,49 @@
 package com.github.tatsuyafujisaki.androidplayground.compose
 
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.StyledPlayerView
 
+private fun Player.autoPlay() {
+    playWhenReady = true
+    prepare()
+}
+
 @Composable
-fun ComposableExoPlayer(uri: String) {
+fun ComposableExoPlayer(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    uri: String
+) {
+    lateinit var styledPlayerView: StyledPlayerView
+
     AndroidView(
         factory = {
             StyledPlayerView(it).apply {
-                player = ExoPlayer.Builder(context).build().apply {
+                styledPlayerView = this
+                player = ExoPlayer.Builder(it).build().apply {
                     setMediaItem(MediaItem.fromUri(uri))
+                    autoPlay()
                 }
             }
-        }
+        },
+        modifier = Modifier.background(Color.Black)
     )
+
+    DisposableEffect(lifecycleOwner) {
+        onDispose {
+            styledPlayerView.player?.release()
+        }
+    }
 }
 
 @Preview
