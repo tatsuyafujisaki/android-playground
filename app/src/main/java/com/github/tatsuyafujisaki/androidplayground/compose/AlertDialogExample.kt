cf35890e656df9d0825deea13ef53972f89c7fc2
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-07-04 11:25:39 +0900

    Create AlertDialogExample.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/AlertDialogExample.kt
@@ -0,0 +1,79 @@
+package com.github.tatsuyafujisaki.androidplayground.compose
+
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AlertDialogExample(
+    text: String,
+    title: String? = null,
+    confirmButtonText: String = stringResource(android.R.string.ok),
+    dismissButtonText: String? = null,
+    canDismiss: Boolean = true,
+    onConfirmButtonClick: () -> Unit = {},
+    onDismiss: () -> Unit = {}
+) {
+    var isVisible by remember { mutableStateOf(true) }
+
+    if (isVisible) {
+        AlertDialog(
+            onDismissRequest = {
+                // ダイアログの外側か戻るボタンがタップされた。
+                if (canDismiss) {
+                    onDismiss()
+                    isVisible = false
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmButtonClick()
+                        isVisible = false
+                    }
+                ) {
+                    Text(confirmButtonText)
+                }
+            },
+            modifier = Modifier.shadow(24.dp),
+            dismissButton = dismissButtonText?.let {
+                {
+                    TextButton(
+                        onClick = {
+                            onDismiss()
+                            isVisible = false
+                        }
+                    ) {
+                        Text(it)
+                    }
+                }
+            },
+            title = title?.let {
+                {
+                    Text(it)
+                }
+            },
+            text = {
+                Text(text)
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewNFrameAlertDialog() {
+    AlertDialogExample(
+        title = "タイトル",
+        text = "テキスト",
+        dismissButtonText = "キャンセル"
+    )
+}
