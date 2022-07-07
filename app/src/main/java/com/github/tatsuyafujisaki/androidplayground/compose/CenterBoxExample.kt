8f91f6e962b1ec50fd842d6366f39ef3c289a62c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-07-07 17:10:26 +0900

    Update CenterBoxExample.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/CenterBoxExample.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/CenterBoxExample.kt
@@ -2,7 +2,12 @@ package com.github.tatsuyafujisaki.androidplayground.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -37,7 +42,7 @@ fun CenterBoxExample(
 
 @Preview(showBackground = true)
 @Composable
-private fun Preview() {
+private fun PreviewCenterBoxExample() {
     CenterBoxExample {
         Text("Sample")
     }
