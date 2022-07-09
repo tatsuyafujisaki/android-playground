2c1c098d84a2d2a7515e7f9f6b5fb66a5cbcc246
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-07-10 00:58:34 +0900

    Create MyWebView.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/MyWebView.kt
@@ -0,0 +1,107 @@
+package com.github.tatsuyafujisaki.androidplayground.compose
+
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun MyWebView(
+    title: String,
+    url: String,
+    popOrFinish: () -> Unit,
+) {
+    var canGoBack by remember { mutableStateOf(false) }
+    var canGoForward by remember { mutableStateOf(false) }
+    var isBackClicked by remember { mutableStateOf(false) }
+    var isReloadClicked by remember { mutableStateOf(false) }
+    var isForwardClicked by remember { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            WebViewTopBar(
+                title = title,
+                canGoBack = canGoBack,
+                canGoForward = canGoForward,
+                onNavigationIconClick = popOrFinish,
+                onBackClick = {
+                    isBackClicked = true
+                },
+                onReloadClick = {
+                    isReloadClicked = true
+                },
+                onForwardClick = {
+                    isForwardClicked = true
+                }
+            )
+        }
+    ) { paddingValues ->
+        AndroidView(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxHeight(),
+            factory = {
+                WebView(it).apply {
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    settings.javaScriptEnabled = true
+                    webViewClient = object : WebViewClient() {
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            canGoBack = canGoBack()
+                            canGoForward = canGoForward()
+                        }
+                    }
+                    loadUrl(url)
+                }
+            },
+            update = {
+                when {
+                    isBackClicked -> {
+                        isBackClicked = false
+                        if (canGoBack) {
+                            it.goBack()
+                        } else {
+                            popOrFinish()
+                        }
+                    }
+                    isReloadClicked -> {
+                        isReloadClicked = false
+                        it.reload()
+                    }
+                    isForwardClicked -> {
+                        isForwardClicked = false
+                        it.goForward()
+                    }
+                }
+            }
+        )
+    }
+    BackHandler {
+        isBackClicked = true
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewMyWebView() {
+    MyWebView(
+        title = "Title",
+        url = "https://example.com",
+        popOrFinish = {},
+    )
+}
