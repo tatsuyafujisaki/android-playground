0f94da13590810d64284808f4f631c6dd7265929
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-08-05 14:58:18 +0900

    Rename MyIconButton to IconButtonExample

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/IconButtonExample.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/IconButtonExample.kt
@@ -6,11 +6,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun MyIconButton(
+fun IconButtonExample(
     modifier: Modifier = Modifier,
+    imageVector: ImageVector,
     onClick: () -> Unit
 ) {
     IconButton(
@@ -18,7 +20,7 @@ fun MyIconButton(
         modifier = modifier
     ) {
         Icon(
-            imageVector = Icons.Default.Favorite,
+            imageVector = imageVector,
             contentDescription = null
         )
     }
@@ -26,6 +28,6 @@ fun MyIconButton(
 
 @Preview
 @Composable
-private fun PreviewMyIconButton() {
-    MyIconButton {}
+private fun PreviewIconButtonExample() {
+    IconButtonExample(imageVector = Icons.Default.Favorite) {}
 }
