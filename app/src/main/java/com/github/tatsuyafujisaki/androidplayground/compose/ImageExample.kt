4cfb729060a8f30ac416c06424e2bcd8a978bdc2
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-08-07 12:56:21 +0900

    Update ImageExample.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/ImageExample.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/ImageExample.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.tatsuyafujisaki.androidplayground.R
 
 @Composable
 fun ImageExample(
@@ -16,4 +18,10 @@ fun ImageExample(
         contentDescription = null,
         modifier = modifier
     )
+}
+
+@Preview
+@Composable
+private fun PreviewImageExample() {
+    ImageExample(R.drawable.) {}
 }
