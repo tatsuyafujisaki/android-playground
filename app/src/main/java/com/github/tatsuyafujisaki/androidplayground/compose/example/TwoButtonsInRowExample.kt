89e8c97f2c313f392cf8c86a70fe4eb40a4e023c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-08-09 13:04:41 +0900

    Create TwoButtonsInRowExample.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/example/TwoButtonsInRowExample.kt
@@ -0,0 +1,41 @@
+package com.github.tatsuyafujisaki.androidplayground.compose.example
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+private fun MyButton(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Button(
+        onClick = {},
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(text = text)
+    }
+}
+
+@Preview
+@Composable
+fun TwoButtonsInRowExample() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        MyButton(
+            modifier = Modifier.weight(1f),
+            text = "a"
+        )
+        Spacer(Modifier.width(8.dp))
+        MyButton(
+            modifier = Modifier.weight(1f),
+            text = "b"
+        )
+    }
+}
