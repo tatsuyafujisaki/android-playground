93527e27f2a7463513cca08f256876140c6e2fd1
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-08-16 01:36:51 +0900

    Delete OnlineCircularImageExample

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/media/OnlineImageExample.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/media/OnlineImageExample.kt
@@ -2,9 +2,12 @@ package com.github.tatsuyafujisaki.androidplayground.compose.media
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -15,16 +18,19 @@ import coil.compose.AsyncImage
 fun OnlineImageExample(
     modifier: Modifier = Modifier,
     url: String,
+    isCircle: Boolean = false,
     border: Pair<Dp, Color>? = null,
     contentScale: ContentScale = ContentScale.Fit
 ) {
+    val m = modifier.clip(CircleShape)
+
     AsyncImage(
         model = url,
         contentDescription = null,
         modifier = if (border != null) {
-            modifier.border(border.first, border.second)
+            m.border(border.first, border.second, if (isCircle) CircleShape else RectangleShape)
         } else {
-            modifier
+            m
         },
         contentScale = contentScale
     )
@@ -41,10 +47,31 @@ private fun PreviewOnlineImageExample() {
 
 @Preview
 @Composable
+private fun PreviewOnlineCircularImageExample() {
+    OnlineImageExample(
+        modifier = Modifier.size(200.dp),
+        url = "https://developer.android.com/static/images/brand/Android_Robot.png",
+        isCircle = true
+    )
+}
+
+@Preview
+@Composable
 private fun PreviewOnlineBorderedImageExample() {
     OnlineImageExample(
         modifier = Modifier.size(200.dp),
         url = "https://developer.android.com/static/images/brand/Android_Robot.png",
+        border = 8.dp to Color.Red
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewOnlineCircularBorderedImageExample() {
+    OnlineImageExample(
+        modifier = Modifier.size(200.dp),
+        url = "https://developer.android.com/static/images/brand/Android_Robot.png",
+        isCircle = true,
         border = 8.dp to Color.Red
     )
 }
