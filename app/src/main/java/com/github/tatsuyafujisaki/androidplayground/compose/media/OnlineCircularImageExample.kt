af914831947673afcfc71eba6c39715c5191fb7b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-08-15 22:17:41 +0900

    Add OnlineCircularImageExample and more

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/media/OnlineCircularImageExample.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/media/OnlineCircularImageExample.kt
@@ -1,8 +1,11 @@
 package com.github.tatsuyafujisaki.androidplayground.compose.media
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -10,7 +13,7 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
 @Composable
-fun WebImageExample(
+fun OnlineCircularImageExample(
     modifier: Modifier = Modifier,
     url: String,
     border: Pair<Dp, Color>
@@ -18,15 +21,18 @@ fun WebImageExample(
     AsyncImage(
         model = url,
         contentDescription = null,
-        modifier = modifier.border(border.first, border.second)
+        modifier = modifier
+            .clip(CircleShape)
+            .border(border.first, border.second, CircleShape)
     )
 }
 
 @Preview
 @Composable
-private fun PreviewWebImageExample() {
-    WebImageExample(
-        url = "https://developer.android.com/static/images/brand/Android_Robot.png",
+private fun PreviewOnlineCircularImageExample() {
+    OnlineCircularImageExample(
+        modifier = Modifier.size(200.dp),
+        url = "https://2.bp.blogspot.com/-3J8MukWVApM/XLAcyuflY4I/AAAAAAABSTA/3IxtQnJKJH48h42rvdg2tGrQEvsc4QxrQCLcBGAs/s800/bg_ocean_suiheisen.jpg",
         border = 8.dp to Color.Red
     )
 }
