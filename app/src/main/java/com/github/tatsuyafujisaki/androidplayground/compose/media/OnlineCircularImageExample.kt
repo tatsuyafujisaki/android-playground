d30c6389deeb071c045742b722c10d2c4e8a5dfb
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-08-15 22:25:55 +0900

    Update OnlineCircularImageExample.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/media/OnlineCircularImageExample.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/media/OnlineCircularImageExample.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -23,7 +24,8 @@ fun OnlineCircularImageExample(
         contentDescription = null,
         modifier = modifier
             .clip(CircleShape)
-            .border(border.first, border.second, CircleShape)
+            .border(border.first, border.second, CircleShape),
+        contentScale = ContentScale.Crop
     )
 }
 
