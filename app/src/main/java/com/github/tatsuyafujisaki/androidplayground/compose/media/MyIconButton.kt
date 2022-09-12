44906519648235e3f5fe2dc3ca50236cd5b8b406
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-09-12 20:21:57 +0900

    Create MyIconButton.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/media/MyIconButton.kt
@@ -0,0 +1,35 @@
+package com.github.tatsuyafujisaki.androidplayground.compose.media
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.tatsuyafujisaki.androidplayground.R
+
+@Composable
+fun MyIconButton(
+    modifier: Modifier = Modifier,
+    @DrawableRes id: Int,
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) {
+    IconButton(
+        modifier = modifier,
+        enabled = enabled,
+        onClick = onClick
+    ) {
+        Image(
+            painter = painterResource(id = id),
+            contentDescription = null
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewMyIconButton() {
+    MyIconButton(id = R.drawable.ic_android_robot) {}
+}
