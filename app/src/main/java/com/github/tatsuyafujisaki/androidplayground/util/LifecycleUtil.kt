797560fb9a56242fb757519590e5dcd745b837a4
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-10-24 14:02:42 +0900

    Update LifecycleUtil.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/LifecycleUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/LifecycleUtil.kt
@@ -2,33 +2,45 @@ package com.github.tatsuyafujisaki.androidplayground.util
 
 import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
 object LifecycleUtil {
-    fun onEvent(fragment: Fragment) {
-        fragment.viewLifecycleOwner.lifecycle
-        fragment.viewLifecycleOwner.lifecycle.addObserver(
-            LifecycleEventObserver { source, event ->
-                Log.d(source::class.java.simpleName, event.name)
+    private val observer1 = object : DefaultLifecycleObserver {
+        override fun onCreate(owner: LifecycleOwner) {}
+        override fun onStart(owner: LifecycleOwner) {}
+        override fun onResume(owner: LifecycleOwner) {}
+        override fun onPause(owner: LifecycleOwner) {}
+        override fun onStop(owner: LifecycleOwner) {}
+        override fun onDestroy(owner: LifecycleOwner) {}
+    }
 
-                when (event) {
-                    Lifecycle.Event.ON_CREATE -> {
-                    }
-                    Lifecycle.Event.ON_START -> {
-                    }
-                    Lifecycle.Event.ON_RESUME -> {
-                    }
-                    Lifecycle.Event.ON_PAUSE -> {
-                    }
-                    Lifecycle.Event.ON_STOP -> {
-                    }
-                    Lifecycle.Event.ON_DESTROY -> {
-                    }
-                    Lifecycle.Event.ON_ANY -> {
-                    }
-                }
-            }
-        )
+    private val observer2 = LifecycleEventObserver { source, event ->
+        Log.d(source::class.java.simpleName, event.name)
+        when (event) {
+            Lifecycle.Event.ON_CREATE -> {}
+            Lifecycle.Event.ON_START -> {}
+            Lifecycle.Event.ON_RESUME -> {}
+            Lifecycle.Event.ON_PAUSE -> {}
+            Lifecycle.Event.ON_STOP -> {}
+            Lifecycle.Event.ON_DESTROY -> {}
+            Lifecycle.Event.ON_ANY -> {}
+        }
+    }
+
+    fun onCreate(fragment: Fragment) {
+        with(fragment.viewLifecycleOwner.lifecycle) {
+            addObserver(observer1)
+            addObserver(observer2)
+        }
+    }
+
+    fun onDestroy(fragment: Fragment) {
+        with(fragment.viewLifecycleOwner.lifecycle) {
+            removeObserver(observer1)
+            removeObserver(observer2)
+        }
     }
 }
