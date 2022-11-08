4676dfd5a65b207202936637bb333a5b4f27ce65
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-11-08 17:19:27 +0900

    Update RobolectricTest.kt

--- app/src/test/java/com/github/tatsuyafujisaki/androidplayground/RobolectricTest.kt
+++ app/src/test/java/com/github/tatsuyafujisaki/androidplayground/RobolectricTest.kt
@@ -1,16 +1,13 @@
 package com.github.tatsuyafujisaki.androidplayground
 
-import android.app.Application
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(application = Application::class)
 class RobolectricTest {
     private val context = ApplicationProvider.getApplicationContext<Context>()
 
