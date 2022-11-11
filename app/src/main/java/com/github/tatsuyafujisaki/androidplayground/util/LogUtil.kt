1529b0845659149de0844bee5c0287d68f838966
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-11-11 10:33:49 +0900

    Update LogUtil.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/LogUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/LogUtil.kt
@@ -3,7 +3,11 @@ package com.github.tatsuyafujisaki.androidplayground.util
 import android.util.Log
 
 object LogUtil {
-    fun log(vararg xs: String) {
+    /**
+     * Usage: log("key1", "value1", "key2", "value2")
+     * Output: [[key1, value1], [key1, value2]]
+     */
+    fun log(vararg xs: Any) {
         Log.d("Here!", xs.toList().chunked(2).toString())
     }
 }
