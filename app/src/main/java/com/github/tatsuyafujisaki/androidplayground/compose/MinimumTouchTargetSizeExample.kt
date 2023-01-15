c089c7925238a583ba0c1902168e8c8e5cbeb48f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-15 20:43:19 +0900

    Create MinimumTouchTargetSizeExample.kt

---
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/MinimumTouchTargetSizeExample.kt
@@ -0,0 +1,25 @@
+package com.github.tatsuyafujisaki.androidplayground.compose
+
+import android.util.Log
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun MinimumTouchTargetSizeExample() {
+    var state by remember { mutableStateOf(false) }
+    RadioButton(selected = state, onClick = { state = true }, modifier = Modifier.onSizeChanged {
+        Log.d("Clickable RadioButton", "Size $it")
+    })
+
+    RadioButton(selected = state, onClick = null, modifier = Modifier.onSizeChanged {
+        Log.d("Not clickable RadioButton", "Size $it")
+    })
+}
