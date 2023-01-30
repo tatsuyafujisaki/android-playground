f83fe8e47621f08b284ab2a15ea5ee99d116629c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-30 20:49:38 +0900

    Update CountDownTimerWrapper.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/flow/CountDownTimerWrapper.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/flow/CountDownTimerWrapper.kt
@@ -3,11 +3,29 @@ package com.github.tatsuyafujisaki.androidplayground.flow
 import android.os.CountDownTimer
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
 
-class CountDownTimerWrapper {
+class CountDownTimerWrapper(
+    millisInFuture: Long,
+    countDownInterval: Long
+) {
     lateinit var countDownTimer: CountDownTimer
+
+    val flow = callbackFlow {
+        countDownTimer =
+            object : CountDownTimer(millisInFuture, countDownInterval) {
+                override fun onTick(millisUntilFinished: Long) {
+                    trySend(millisUntilFinished)
+                }
+
+                override fun onFinish() {
+                    println("onFinish")
+                }
+            }
+        awaitClose {}
+    }
 
     fun start() {
         countDownTimer.start()
@@ -18,32 +36,15 @@ class CountDownTimerWrapper {
     }
 }
 
-private fun flowFrom(
-    countDownTimerWrapper: CountDownTimerWrapper,
-    millisInFuture: Long,
-    countDownInterval: Long
-) = callbackFlow {
-    countDownTimerWrapper.countDownTimer =
-        object : CountDownTimer(millisInFuture, countDownInterval) {
-            override fun onTick(millisUntilFinished: Long) {
-                trySend(millisUntilFinished)
-            }
-
-            override fun onFinish() {
-                println("onFinish")
-            }
-        }
-    awaitClose {}
-}
-
 private suspend fun main() = coroutineScope {
-    val countDownTimerWrapper = CountDownTimerWrapper()
+    val countDownTimerWrapper = CountDownTimerWrapper(3_000, 1_000)
 
     launch {
-        flowFrom(countDownTimerWrapper, 3_000, 1_000).collect {
+        countDownTimerWrapper.flow.collect {
             println("collect: $it")
         }
     }
 
+    delay(1_000)
     countDownTimerWrapper.start()
 }
