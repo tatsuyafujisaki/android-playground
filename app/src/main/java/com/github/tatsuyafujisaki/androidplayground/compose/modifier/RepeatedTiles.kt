43bdce525ddd7b0950bf3df85bf45286685dee7d
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-02-27 21:10:14 +0900

    Add RepeatedTiles

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/modifier/RepeatedTiles.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/modifier/RepeatedTiles.kt
@@ -1,11 +1,16 @@
-package com.github.tatsuyafujisaki.androidplayground.compose.spacer
+package com.github.tatsuyafujisaki.androidplayground.compose.modifier
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.ImageShader
 import androidx.compose.ui.graphics.ShaderBrush
@@ -17,21 +22,36 @@ import com.github.tatsuyafujisaki.androidplayground.R
 /**
  * @see [Stack Overflow](https://stackoverflow.com/a/73821217/10867055)
  */
-@Preview
-@Composable
-private fun RepeatedTileSpacerExample() {
+private fun Modifier.repeatedTiles(): Modifier = composed {
     val imageBitmap = ImageBitmap.imageResource(R.drawable.ichimatsu)
     val shaderBrush = remember(imageBitmap) {
         ShaderBrush(
             ImageShader(
-                imageBitmap, TileMode.Repeated, TileMode.Repeated
+                image = imageBitmap, tileModeX = TileMode.Repeated, tileModeY = TileMode.Repeated
             )
         )
     }
+    background(shaderBrush)
+}
 
+@Preview
+@Composable
+private fun RepeatedTileSpacerExample() {
     Spacer(
         Modifier
+            .repeatedTiles()
             .fillMaxSize()
-            .background(shaderBrush)
     )
+}
+
+@Preview
+@Composable
+private fun RepeatedTileBoxPreview() {
+    Box(
+        modifier = Modifier
+            .repeatedTiles()
+            .fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Hello", color = Color.White)
+    }
 }
