82199b0ddc20e7db1f5c7c00ecfe1d0a987b88c9
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-03-15 07:43:03 +0900

    Update FragmentUtil.kt

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/FragmentUtil.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/util/FragmentUtil.kt
@@ -1,8 +1,6 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
-import android.util.Log
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentManager.BackStackEntry
 
@@ -20,21 +18,11 @@ object FragmentUtil {
             .map {
                 val tag = getBackStackEntryAt(it).name
                 /**
-                 * [FragmentManager.findFragmentByTag] returns null if the [BackStackEntry] has NOT been added with the tag.
+                 * [FragmentManager.findFragmentByTag returns null
+                 *   if the [BackStackEntry] has NOT been added with the tag.
                  */
                 findFragmentByTag(tag)?.javaClass?.simpleName ?: tag
             }
-
-    fun Fragment.logFragmentManagers() {
-        fun FragmentManager.log(tagPrefix: String) {
-            Log.d("$tagPrefix.fragments", fragmentNames.joinToString(","))
-            Log.d("$tagPrefix.backStackEntries", backStackEntryNames.joinToString(","))
-        }
-
-        val fragmentName = javaClass.simpleName
-        parentFragmentManager.log("$fragmentName.parentFragmentManager")
-        childFragmentManager.log("$fragmentName.childFragmentManager")
-    }
 
     fun FragmentManager.popBackStack(fragmentCountToPop: Int) {
         popBackStack(
