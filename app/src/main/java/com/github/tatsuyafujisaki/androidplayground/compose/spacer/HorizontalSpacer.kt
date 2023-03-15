2ca543df233309a28006e2c444dca0468d35f9ba
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-03-15 16:56:07 +0900

    Update HorizontalSpacerPreview and VerticalSpacerPreview

--- app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/spacer/HorizontalSpacer.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/androidplayground/compose/spacer/HorizontalSpacer.kt
@@ -5,20 +5,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-
-@Composable
-fun HorizontalSpacer(width: Dp) {
-    Spacer(Modifier.width(width))
-}
 
 @Preview(showBackground = true)
 @Composable
 private fun HorizontalSpacerPreview() {
     Column {
         Text("Hello")
-        HorizontalSpacer(8.dp)
+        Spacer(Modifier.width(8.dp))
         Text("World")
     }
 }
