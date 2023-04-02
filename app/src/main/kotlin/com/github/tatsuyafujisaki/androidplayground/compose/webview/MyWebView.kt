58aa432e9c8941ec77f073dae799c9cc13ecc1ff
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-04-02 13:59:56 +0900

    Update MyWebView.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/compose/webview/MyWebView.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/compose/webview/MyWebView.kt
@@ -23,10 +23,7 @@ import com.github.tatsuyafujisaki.androidplayground.compose.WebViewTopBar
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun MyWebView(
-    title: String,
-    url: String,
-    popOrFinish: () -> Unit,
-    onClick: (() -> Unit)? = null
+    title: String, url: String, popOrFinish: () -> Unit, onClick: (() -> Unit)? = null
 ) {
     var canGoBack by remember { mutableStateOf(false) }
     var canGoForward by remember { mutableStateOf(false) }
@@ -34,77 +31,60 @@ fun MyWebView(
     var isReloadClicked by remember { mutableStateOf(false) }
     var isForwardClicked by remember { mutableStateOf(false) }
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            WebViewTopBar(
-                title = title,
-                canGoBack = canGoBack,
-                canGoForward = canGoForward,
-                onNavigationIconClick = popOrFinish,
-                onBackClick = {
-                    isBackClicked = true
-                },
-                onReloadClick = {
-                    isReloadClicked = true
-                },
-                onForwardClick = {
-                    isForwardClicked = true
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        WebViewTopBar(title = title,
+            canGoBack = canGoBack,
+            canGoForward = canGoForward,
+            onNavigationIconClick = popOrFinish,
+            onBackClick = { isBackClicked = true },
+            onReloadClick = { isReloadClicked = true },
+            onForwardClick = { isForwardClicked = true })
+    }) { paddingValues ->
+        AndroidView(modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize(), factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                settings.javaScriptEnabled = true
+                webViewClient = object : WebViewClient() {
+                    override fun doUpdateVisitedHistory(
+                        view: WebView?, url: String?, isReload: Boolean
+                    ) {
+                        canGoBack = canGoBack()
+                        canGoForward = canGoForward()
+                    }
                 }
-            )
-        }
-    ) { paddingValues ->
-        AndroidView(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize(),
-            factory = {
-                WebView(it).apply {
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                    )
-                    settings.javaScriptEnabled = true
-                    webViewClient = object : WebViewClient() {
-                        override fun doUpdateVisitedHistory(
-                            view: WebView?,
-                            url: String?,
-                            isReload: Boolean
-                        ) {
-                            canGoBack = canGoBack()
-                            canGoForward = canGoForward()
-                        }
+                onClick?.let {
+                    setOnTouchListener { v, _ ->
+                        it.invoke()
+                        v.performClick()
                     }
-                    onClick?.let {
-                        setOnTouchListener { v, _ ->
-                            it.invoke()
-                            v.performClick()
-                        }
-                    }
-                    loadUrl(url)
                 }
-            },
-            update = {
-                when {
-                    isBackClicked -> {
-                        isBackClicked = false
-                        if (canGoBack) {
-                            it.goBack()
-                        } else {
-                            popOrFinish()
-                        }
+                loadUrl(url)
+            }
+        }, update = {
+            when {
+                isBackClicked -> {
+                    isBackClicked = false
+                    if (canGoBack) {
+                        it.goBack()
+                    } else {
+                        popOrFinish()
                     }
-                    isReloadClicked -> {
-                        isReloadClicked = false
-                        it.reload()
-                    }
-                    isForwardClicked -> {
-                        isForwardClicked = false
-                        it.goForward()
-                    }
+                }
+                isReloadClicked -> {
+                    isReloadClicked = false
+                    it.reload()
+                }
+                isForwardClicked -> {
+                    isForwardClicked = false
+                    it.goForward()
                 }
             }
-        )
+            it.loadUrl(url)
+        })
     }
     BackHandler {
         isBackClicked = true
