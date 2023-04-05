7e4cc84980dcb523daaeddfd76a3a29bf9f4c9c7
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-04-05 12:57:00 +0900

    Update NetworkUtil.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/NetworkUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/NetworkUtil.kt
@@ -5,15 +5,34 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import okhttp3.HttpUrl
 
+enum class NetworkType {
+    WIFI,
+    CELLULAR,
+    UNKNOWN
+}
+
 object NetworkUtil {
     fun isNetworkAvailable(context: Context) =
         with(context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager) {
             getNetworkCapabilities(activeNetwork)?.run {
-                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || hasTransport(
-                    NetworkCapabilities.TRANSPORT_WIFI
-                ) || hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
             } ?: false
         }
+
+    fun getNetworkType(context: Context): NetworkType {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+
+        return when {
+            capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true -> NetworkType.WIFI
+            capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true -> NetworkType.CELLULAR
+            else -> NetworkType.UNKNOWN
+        }
+    }
 
     private fun getBaseUrl(url: HttpUrl) =
         HttpUrl.Builder().scheme(url.scheme).host(url.host).build()
