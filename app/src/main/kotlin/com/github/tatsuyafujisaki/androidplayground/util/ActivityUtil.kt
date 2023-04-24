5159b66cd5f9b676ef51d6ac1317417510efb648
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-04-24 21:30:37 +0900

    Update ActivityUtil.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/ActivityUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/ActivityUtil.kt
@@ -2,11 +2,9 @@ package com.github.tatsuyafujisaki.androidplayground.util
 
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
 import android.util.Log
 import android.view.KeyEvent
 import android.view.View
-import android.view.Window
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.ComponentActivity
@@ -27,21 +25,21 @@ object ActivityUtil {
 
         fun showKeyboard(activity: Activity) {
             WindowInsetsControllerCompat(
-                activity.window,
-                activity.window.decorView
+                activity.window, activity.window.decorView
             ).show(Type.ime())
         }
 
         private fun hideKeyboard(activity: Activity) {
             WindowInsetsControllerCompat(
-                activity.window,
-                activity.window.decorView
+                activity.window, activity.window.decorView
             ).hide(Type.ime())
         }
 
         fun hideKeyboardOldWay(activity: Activity) {
-            (activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
-                .hideSoftInputFromWindow(activity.currentFocus?.windowToken, 0)
+            (activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
+                    activity.currentFocus?.windowToken,
+                    0
+                )
         }
 
         fun hideKeyboardOnEnter(activity: Activity) {
@@ -60,26 +58,26 @@ object ActivityUtil {
         /**
          * <a href="https://developer.android.com/develop/ui/views/layout/immersive">Hide system bars for immersive mode</a>
          */
-        fun fullscreen(window: Window) {
+        fun fullscreen(activity: Activity) {
+            val window = activity.window
             with(WindowCompat.getInsetsController(window, window.decorView)) {
                 // Prevent only touching from showing the system bars.
-                systemBarsBehavior = WindowInsetsControllerCompat
-                    .BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
                 hide(Type.systemBars())
             }
         }
 
-        fun exitFullscreen(window: Window) {
+        fun exitFullscreen(activity: Activity) {
+            val window = activity.window
             WindowCompat.getInsetsController(window, window.decorView).show(Type.systemBars())
         }
     }
 
-    object ScreenAwake {
-        fun keepScreenOn(activity: Activity) {
+    fun keepScreenOn(activity: Activity, on: Boolean) {
+        if (on) {
             activity.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        }
-
-        fun stopKeepingScreenOn(activity: Activity) {
+        } else {
             activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         }
     }
@@ -95,10 +93,8 @@ object ActivityUtil {
         activity.intent?.extras?.getString(key).orEmpty()
 
     fun logStateChanged(activity: ComponentActivity, tag: String) {
-        activity.lifecycle.addObserver(
-            LifecycleEventObserver { _, event ->
-                Log.d(tag, event.toString())
-            }
-        )
+        activity.lifecycle.addObserver(LifecycleEventObserver { _, event ->
+            Log.d(tag, event.toString())
+        })
     }
 }
