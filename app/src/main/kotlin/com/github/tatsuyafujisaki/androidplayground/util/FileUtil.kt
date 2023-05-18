9ec8e573dc27961bb7c258b499988740e57f1c70
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-05-18 22:15:54 +0900

    Delete FileUtil.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/FileUtil.kt
+++
@@ -1,9 +0,0 @@
-package com.github.tatsuyafujisaki.androidplayground.util
-
-import java.io.File
-import java.net.URLEncoder
-
-object FileUtil {
-    val File.encoded
-        get() = URLEncoder.encode(name, "UTF-8")
-}
