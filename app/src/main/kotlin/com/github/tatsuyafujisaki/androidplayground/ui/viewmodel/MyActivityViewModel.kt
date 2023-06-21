1f34d7f694c41f9a4a4cf2d990100b511730f782
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-06-21 18:53:46 +0900

    Create MyActivityViewModel.kt

---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/viewmodel/MyActivityViewModel.kt
@@ -0,0 +1,28 @@
+package com.github.tatsuyafujisaki.androidplayground.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MyActivityViewModel : ViewModel() {
+    private val _stateFlow = MutableStateFlow("")
+    val stateFlow: StateFlow<String> = _stateFlow.asStateFlow()
+
+    private val _myLiveData = MutableLiveData("")
+    val myLiveData: LiveData<String> = _myLiveData
+
+    fun setMyStateFlow(something: String) {
+        _stateFlow.value = something
+    }
+
+    fun setMyLiveData(something: String) {
+        _myLiveData.value = something
+    }
+
+    companion object {
+        private const val TAG = "MyActivityViewModel"
+    }
+}
