d90dfcb8ca772287b041f144500538a95096a035
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-06-21 18:52:48 +0900

    Update MainViewModel.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/viewmodel/MainViewModel.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/viewmodel/MainViewModel.kt
@@ -1,21 +1,25 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
-    private val _something = MutableLiveData("")
-    val something: LiveData<String> = _something
+    private val _stateFlow = MutableStateFlow("")
+    val stateFlow: StateFlow<String> = _stateFlow.asStateFlow()
 
-    fun setSomething(something: String) {
-        _something.value = something
+    private val _myLiveData = MutableLiveData("")
+    val myLiveData: LiveData<String> = _myLiveData
+
+    fun setMyStateFlow(something: String) {
+        _stateFlow.value = something
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.d(TAG, object {}.javaClass.enclosingMethod!!.name)
+    fun setMyLiveData(something: String) {
+        _myLiveData.value = something
     }
 
     companion object {
