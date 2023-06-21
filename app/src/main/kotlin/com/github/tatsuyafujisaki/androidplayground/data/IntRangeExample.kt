a8d075bc3675d2ca60b9db5e80fded568f152ebd
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-06-21 19:40:35 +0900

    Rename SampleRange to IntRangeExample

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/IntRangeExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/IntRangeExample.kt
@@ -7,7 +7,7 @@ import androidx.annotation.IntRange
  * On a separate note, you cannot validate "lower <= upper" with @IntRange
  * because @IntRange can take only compile-time constants.
  */
-data class SampleRange(
+data class IntRangeExample(
     @IntRange(from = MIN, to = MAX) val lower: Int,
     @IntRange(from = MIN, to = MAX) val upper: Int
 ) {
@@ -17,6 +17,6 @@ data class SampleRange(
 
     companion object {
         const val MIN = 0L
-        const val MAX = 10000L
+        const val MAX = 100L
     }
 }
