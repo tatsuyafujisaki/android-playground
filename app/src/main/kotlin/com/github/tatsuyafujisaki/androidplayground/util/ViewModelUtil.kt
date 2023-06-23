ee02a13709b71dfdc294f2fef853598895d95020
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-06-23 10:18:20 +0900

    Update ViewModelUtil.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/ViewModelUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/ViewModelUtil.kt
@@ -2,8 +2,22 @@ package com.github.tatsuyafujisaki.androidplayground.util
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 
 object ViewModelUtil {
+    /**
+     * https://developer.android.com/topic/libraries/architecture/viewmodel/viewmodel-factories
+     */
+    inline fun <reified VM : ViewModel> createViewModelProviderFactory(crossinline initializer: CreationExtras.() -> VM) =
+        viewModelFactory {
+            initializer {
+                initializer()
+            }
+        }
+
+    @Deprecated(message = "factory for view model version older than 2.5.0")
     fun <V : ViewModel, T> createViewModelProviderFactory(
         create: (T) -> V
     ): (T) -> ViewModelProvider.Factory = {
