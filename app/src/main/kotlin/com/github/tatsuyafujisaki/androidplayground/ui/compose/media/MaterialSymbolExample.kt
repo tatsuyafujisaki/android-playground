b666d8119f23c5bf0447ef2bb1fd7318cd750d79
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-06-28 13:54:46 +0900

    Create MaterialSymbolExample.kt

---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/MaterialSymbolExample.kt
@@ -0,0 +1,18 @@
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.media
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun MaterialSymbolExamplePreview() {
+    Icon(
+        imageVector = Icons.Default.Favorite,
+        contentDescription = null,
+        tint = Color.Red
+    )
+}
