cd15bc754493cd1b390fdafacf7f180f3eeb8fa7
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-07-03 10:01:51 +0900

    Update ParcelableTest.kt

--- app/src/test/kotlin/com/github/tatsuyafujisaki/androidplayground/ParcelableTest.kt
+++ app/src/test/kotlin/com/github/tatsuyafujisaki/androidplayground/ParcelableTest.kt
@@ -2,16 +2,17 @@ package com.github.tatsuyafujisaki.androidplayground
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.ibm.icu.impl.PluralRulesLoader
 import kotlin.test.assertEquals
 
 object ParcelableTest {
     fun <T : Parcelable> assertParcelable(original: T) {
-        assertEquals(original, saveAndRead(original))
+        assertEquals(original, writeAndRead(original))
     }
 
-    private fun <T : Parcelable> saveAndRead(data: T): T = with(Parcel.obtain()) {
+    private fun <T : Parcelable> writeAndRead(data: T): T = with(Parcel.obtain()) {
         writeParcelable(data, 0)
         setDataPosition(0)
-        readParcelable(data.javaClass.classLoader)!!
+        readParcelable(PluralRulesLoader.loader.javaClass.classLoader)!!
     }
 }
