5a830fb0961aa8aaa90021b86e2df40fe75e89d1
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-07-25 12:36:02 +0900

    Add takeScreenshot(view: View)

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/GraphicsUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/GraphicsUtil.kt
@@ -2,10 +2,13 @@ package com.github.tatsuyafujisaki.androidplayground.util
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.ImageDecoder
 import android.graphics.Matrix
 import android.os.Build
 import android.provider.MediaStore
+import android.view.View
 import androidx.core.net.toUri
 import java.lang.Integer.max
 
@@ -27,7 +30,7 @@ object GraphicsUtil {
         originalImageWidth: Int,
         originalImageHeight: Int,
         requiredWidth: Int,
-        requiredHeight: Int
+        requiredHeight: Int,
     ): Int {
         var inSampleSize = 1
         // Calculate the largest inSampleSize value that is a power of 2 and keeps both height and width larger than the requested height and width.
@@ -39,8 +42,14 @@ object GraphicsUtil {
         return max(1, inSampleSize / 2)
     }
 
-    fun rotateClockwise(bitmap: Bitmap, degrees: Float) = Bitmap.createBitmap(
+    fun rotateClockwise(bitmap: Bitmap, degrees: Float): Bitmap = Bitmap.createBitmap(
         bitmap, 0, 0, bitmap.width, bitmap.height,
         Matrix().apply { postRotate(degrees) }, true
     )
+
+    fun takeScreenshot(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        view.draw(Canvas(bitmap).apply { drawColor(Color.WHITE) })
+        return bitmap
+    }
 }
