1c0c7f97c920a5386e2c2c52a0973507ed05b60d
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-08-08 10:12:51 +0900

    Define StatelessPullRefreshBox and StatefulPullRefreshBox

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/StatefulPullRefreshBox.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/StatefulPullRefreshBox.kt
@@ -4,20 +4,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
-import androidx.compose.material.pullrefresh.PullRefreshState
 import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun PullRefreshBox(
+fun StatefulPullRefreshBox(
     refreshing: Boolean,
-    state: PullRefreshState,
+    onRefresh: () -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable BoxScope.() -> Unit,
 ) {
+    val state = rememberPullRefreshState(refreshing, onRefresh)
+
     Box(modifier = modifier.pullRefresh(state)) {
         content()
         PullRefreshIndicator(
