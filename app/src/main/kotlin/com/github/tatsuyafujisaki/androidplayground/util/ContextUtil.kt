085ad567db29c9302b318a0863a985978dbc0e99
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-08-19 09:25:52 +0900

    Update ContextUtil.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/ContextUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/ContextUtil.kt
@@ -1,31 +1,27 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.res.Resources
 import android.provider.Settings
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.MenuItem
-import android.view.View
 import android.view.View.TEXT_ALIGNMENT_CENTER
-import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
-import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getColorStateList
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.net.toUri
-import androidx.fragment.app.Fragment
 import com.github.tatsuyafujisaki.androidplayground.BuildConfig
 import com.github.tatsuyafujisaki.androidplayground.R
 import com.google.android.material.chip.Chip
-import com.google.android.material.snackbar.Snackbar
 import java.io.BufferedReader
+
+private const val TAG = "ContextUtil"
 
 /**
  * Impractical redundant explanatory wrappers
@@ -33,8 +29,7 @@ import java.io.BufferedReader
 object ContextUtil {
     object AutoRotation {
         fun isAutoRotatable(context: Context) = Settings.System.getInt(
-            context.contentResolver,
-            Settings.System.ACCELEROMETER_ROTATION
+            context.contentResolver, Settings.System.ACCELEROMETER_ROTATION
         ) == 1
     }
 
@@ -43,11 +38,7 @@ object ContextUtil {
      * cf. [ResourcesUtil.readResourceAsText]
      */
     fun readAssetAsText(context: Context, fileName: String) =
-        context
-            .assets
-            .open(fileName)
-            .bufferedReader()
-            .use(BufferedReader::readText)
+        context.assets.open(fileName).bufferedReader().use(BufferedReader::readText)
 
     fun openInBrowser(context: Context, url: String) {
         startActivity(context, Intent(Intent.ACTION_VIEW, url.toUri()), null)
@@ -57,20 +48,20 @@ object ContextUtil {
      * https://developer.android.com/distribute/marketing-tools/linking-to-google-play#android-app
      */
     fun openGooglePlay(context: Context) {
-        context.startActivity(
-            Intent(Intent.ACTION_VIEW).apply {
-                data =
-                    ("https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID).toUri()
-                setPackage("com.android.vending")
-            }
-        )
+        context.startActivity(Intent(Intent.ACTION_VIEW).apply {
+            data =
+                ("https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID).toUri()
+            setPackage("com.android.vending")
+        })
     }
 
-    // Usage: logScreenInfo(resources.displayMetrics)
-    fun DisplayMetrics.logScreenInfo() {
-        require(density == densityDpi / 160f)
+    /**
+     * @param displayMetrics is a property of [Resources]
+     */
+    fun printScreenInfo(displayMetrics: DisplayMetrics) {
+        require(displayMetrics.density == displayMetrics.densityDpi / 160f)
 
-        val densityQualifier = densityDpi.let {
+        val densityQualifier = displayMetrics.densityDpi.let {
             when {
                 it <= DisplayMetrics.DENSITY_LOW -> "ldpi (~120dpi)"
                 it <= DisplayMetrics.DENSITY_MEDIUM -> "mdpi (121~160dpi)"
@@ -82,44 +73,23 @@ object ContextUtil {
             }
         }
 
-        val tag = "LogScreenInfo"
-
-        Log.d(tag, "dpi: $densityDpi")
-        Log.d(tag, "density [(# of 160px) / inch] (= dpi / 160): $density")
-        Log.d(tag, "densityQualifier: $densityQualifier")
-        Log.d(tag, "widthPixels: $widthPixels")
-        Log.d(tag, "heightPixels: $heightPixels")
+        Log.d(TAG, "dpi: $${displayMetrics.densityDpi}")
+        Log.d(TAG, "density [(# of 160px) / inch] (= dpi / 160): ${displayMetrics.density}")
+        Log.d(TAG, "densityQualifier: $densityQualifier")
+        Log.d(TAG, "widthPixels: ${displayMetrics.widthPixels}")
+        Log.d(TAG, "heightPixels: ${displayMetrics.heightPixels}")
         Log.d(
-            tag,
-            "widthInDp [(1 / 160) inch] (= px / density): " + "${(widthPixels / density).toInt()}"
+            TAG,
+            "widthInDp [(1 / 160) inch] (= px / density): ${(displayMetrics.widthPixels / displayMetrics.density).toInt()}"
         )
         Log.d(
-            tag,
-            "heightInDp [(1 / 160) inch] (= px / density): " + "${(heightPixels / density).toInt()}"
+            TAG,
+            "heightInDp [(1 / 160) inch] (= px / density): ${(displayMetrics.heightPixels / displayMetrics.density).toInt()}"
         )
     }
 
-    /**
-     * "Android also provides a Toast class with a similar API that can be used
-     * for displaying system-level notifications.
-     * Generally, snackbars are the preferred mechanism for displaying feedback messages to users,
-     * as they can be displayed in the context of the UI where the action occurred."
-     * quoted from https://material.io/develop/android/components/snackbar
-     */
-    fun View.snackbar(@StringRes resId: Int) =
-        Snackbar.make(this, resId, Snackbar.LENGTH_SHORT).show()
-
-    fun View.snackbar(text: String) =
-        Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
-
-    fun Context.toast(@StringRes resId: Int) =
-        Toast.makeText(this, resId, Toast.LENGTH_SHORT).show()
-
-    fun Context.toast(text: String) =
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-
-    fun <T> Context.createChip(text: String, tag: T): Chip {
-        return Chip(this).apply {
+    fun <T> createChip(context: Context, text: String, tag: T): Chip {
+        return Chip(context).apply {
             this.text = text
             this.tag = tag
             chipBackgroundColor = getColorStateList(context, R.color.checked_state_selector)
@@ -132,23 +102,15 @@ object ContextUtil {
         }
     }
 
-    fun MenuItem.color(@ColorInt color: Int) {
-        title = SpannableString(title.toString()).apply {
+    fun color(menuItem: MenuItem, @ColorInt color: Int) {
+        menuItem.title = SpannableString(menuItem.title.toString()).apply {
             setSpan(ForegroundColorSpan(color), 0, length, 0)
         }
     }
 
-    fun MenuItem.color(context: Context, @ColorRes id: Int) {
-        title = SpannableString(title.toString()).apply {
+    fun color(context: Context, menuItem: MenuItem, @ColorRes id: Int) {
+        menuItem.title = SpannableString(menuItem.title.toString()).apply {
             setSpan(ForegroundColorSpan(ContextCompat.getColor(context, id)), 0, length, 0)
         }
-    }
-
-    fun MenuItem.color2(activity: Activity, @ColorInt color: Int) {
-        activity.findViewById<TextView>(itemId)?.setTextColor(color)
-    }
-
-    fun MenuItem.color3(fragment: Fragment, @ColorInt color: Int) {
-        fragment.view?.findViewById<TextView>(itemId)?.setTextColor(color)
     }
 }
