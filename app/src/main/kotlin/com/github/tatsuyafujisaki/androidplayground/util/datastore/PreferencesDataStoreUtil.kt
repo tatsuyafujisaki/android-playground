5d451c9ee8a4147b08b0773d5d08789955e86fa8
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-02-04 16:11:33 +0900

    Update PreferencesDataStoreUtil.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/datastore/PreferencesDataStoreUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/datastore/PreferencesDataStoreUtil.kt
@@ -1,4 +1,4 @@
-package com.github.tatsuyafujisaki.androidplayground.util
+package com.github.tatsuyafujisaki.androidplayground.util.datastore
 
 import android.content.Context
 import androidx.datastore.core.DataStore
@@ -8,17 +8,17 @@ import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore("")
+val Context.preferencesDataStore: DataStore<Preferences> by preferencesDataStore("")
 
-object DataStoreUtil {
+object PreferencesDataStoreUtil {
     suspend fun <T> get(context: Context, key: Preferences.Key<T>, defaultValue: T) =
-        context.dataStore.data.map { it[key] ?: defaultValue }.first()
+        context.preferencesDataStore.data.map { it[key] ?: defaultValue }.first()
 
     suspend fun <T> set(context: Context, key: Preferences.Key<T>, value: T) = runCatching {
-        context.dataStore.edit { it[key] = value }
+        context.preferencesDataStore.edit { it[key] = value }
     }.isSuccess
 
     suspend fun <T> remove(context: Context, key: Preferences.Key<T>) = runCatching {
-        context.dataStore.edit { it.remove(key) }
+        context.preferencesDataStore.edit { it.remove(key) }
     }.isSuccess
 }
