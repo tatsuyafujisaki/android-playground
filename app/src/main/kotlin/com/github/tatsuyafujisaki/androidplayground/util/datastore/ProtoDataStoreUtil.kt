9363d96be95f1cf129599c6e0aac6aa2abe86b65
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-02-04 16:53:44 +0900

    Update ProtoDataStoreUtil.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/datastore/ProtoDataStoreUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/datastore/ProtoDataStoreUtil.kt
@@ -1,3 +1,33 @@
 package com.github.tatsuyafujisaki.androidplayground.util.datastore
 
-object ProtoDataStoreUtil
+import MyPersonOuterClass.MyPerson
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object MyPersonSerializer : Serializer<MyPerson> {
+    override val defaultValue = MyPerson.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): MyPerson {
+        try {
+            return MyPerson.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(
+        t: MyPerson,
+        output: OutputStream,
+    ) = t.writeTo(output)
+}
+
+val Context.myPersonDataStore: DataStore<MyPerson> by dataStore(
+    fileName = "my_person.pb",
+    serializer = MyPersonSerializer
+)
