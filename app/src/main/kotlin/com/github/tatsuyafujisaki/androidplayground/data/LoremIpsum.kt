6b47d62c4231ee51512990d37f3c8cd8ec615e71
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-06 20:48:57 +0900

    Update LoremIpsum.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/LoremIpsum.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/LoremIpsum.kt
@@ -2,5 +2,11 @@ package com.github.tatsuyafujisaki.androidplayground.data
 
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 
-fun generateLoremIpsum() = LoremIpsum().values.joinToString()
-fun generateLoremIpsum(words: Int) = LoremIpsum(words).values.joinToString()
+fun generateLoremIpsum() = LoremIpsum().values.first()
+fun generateLoremIpsum(words: Int) = LoremIpsum(words).values.first()
+
+private fun main() {
+    println(generateLoremIpsum())
+    println("--")
+    println(generateLoremIpsum(5))
+}
