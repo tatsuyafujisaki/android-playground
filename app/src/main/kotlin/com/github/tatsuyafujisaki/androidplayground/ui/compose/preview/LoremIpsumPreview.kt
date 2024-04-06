98ff792856225c4e17cd183ad807155c463046ef
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-06 19:03:02 +0900

    Update LoremIpsumPreview.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/preview/LoremIpsumPreview.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/preview/LoremIpsumPreview.kt
@@ -6,10 +6,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 
+private class LoremIpsum8 : LoremIpsum(words = 8)
 private class LoremIpsum18 : LoremIpsum(words = 18)
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-private fun LoremIpsumPreview(@PreviewParameter(LoremIpsum18::class) text: String) {
-    Text(text)
+private fun LoremIpsum8Preview(@PreviewParameter(LoremIpsum8::class) text: String) {
+    Text(text = text)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoremIpsum18Preview(@PreviewParameter(LoremIpsum18::class) text: String) {
+    Text(text = text)
 }
