755b78437a2c7c1ad2d1f18a2704aac3390657c4
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-07 12:03:37 +0900

    Update CenterRowExample.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/row/CenterRowExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/row/CenterRowExample.kt
@@ -1,7 +1,6 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.row
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -9,23 +8,19 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Android
-import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun CenterRowExample(
-    background: Color = Color.White,
     onClick: (() -> Unit)? = null,
     content: @Composable RowScope.() -> Unit,
 ) {
     Row(
         modifier = Modifier
             .fillMaxSize()
-            .background(background)
             .clickable(enabled = onClick != null, onClick = onClick ?: {}),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
