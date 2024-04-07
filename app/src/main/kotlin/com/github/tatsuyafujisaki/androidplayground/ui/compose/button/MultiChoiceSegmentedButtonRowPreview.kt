fa78b90e8c987868546ba91ccb99bf71ea7f30a8
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-07 13:32:12 +0900

    Create MultiChoiceSegmentedButtonRowPreview.kt

---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/button/MultiChoiceSegmentedButtonRowPreview.kt
@@ -0,0 +1,56 @@
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.button
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.MusicNote
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MultiChoiceSegmentedButtonRow
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+private fun MultiChoiceSegmentedButtonRowPreview() {
+    val options = listOf(
+        Icons.Default.Favorite to "Heart",
+        Icons.Default.MusicNote to "Music",
+        Icons.Default.Star to "Star"
+    )
+    val selectedIndices = remember { mutableStateListOf<Int>() }
+    MultiChoiceSegmentedButtonRow {
+        options.forEachIndexed { index, (imageVector, text) ->
+            SegmentedButton(
+                checked = index in selectedIndices,
+                onCheckedChange = {
+                    if (index in selectedIndices) {
+                        selectedIndices.remove(index)
+                    } else {
+                        selectedIndices.add(index)
+                    }
+                },
+                shape = SegmentedButtonDefaults.itemShape(index = index, count = options.size),
+                icon = {
+                    SegmentedButtonDefaults.Icon(active = index in selectedIndices) {
+                        Icon(
+                            imageVector = imageVector,
+                            contentDescription = null,
+                            modifier = Modifier.size(SegmentedButtonDefaults.IconSize)
+                        )
+                    }
+                },
+            ) {
+                Text(text = text)
+            }
+        }
+    }
+}
