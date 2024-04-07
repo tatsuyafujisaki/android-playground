f5b05cbd0edeaa550b983e419ef5f85c554d2b56
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-07 12:01:29 +0900

    Update CenterRowExample.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/row/CenterRowExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/row/CenterRowExample.kt
@@ -1,12 +1,15 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.row
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Android
+import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,7 +20,7 @@ import androidx.compose.ui.tooling.preview.Preview
 fun CenterRowExample(
     background: Color = Color.White,
     onClick: (() -> Unit)? = null,
-    content: @Composable RowScope.() -> Unit
+    content: @Composable RowScope.() -> Unit,
 ) {
     Row(
         modifier = Modifier
@@ -34,8 +37,11 @@ fun CenterRowExample(
 @Composable
 private fun CenterRowExamplePreview() {
     CenterRowExample {
-        Text("Bacon")
-        Text("Lettuce")
-        Text("Tomato")
+        repeat(3) {
+            Image(
+                imageVector = Icons.Default.Android,
+                contentDescription = null
+            )
+        }
     }
 }
