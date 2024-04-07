3529427a89c76999888e9283bf07be1d30d75914
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-07 13:27:27 +0900

    Update SingleChoiceSegmentedButtonRowPreview.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/button/SingleChoiceSegmentedButtonRowPreview.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/button/SingleChoiceSegmentedButtonRowPreview.kt
@@ -1,6 +1,12 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.button
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.MusicNote
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
@@ -10,16 +16,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 private fun SingleChoiceSegmentedButtonRowPreview() {
-    val options = listOf("Bacon", "Lettuce", "Tomato")
+    val options = listOf(
+        Icons.Default.Favorite to "Heart",
+        Icons.Default.MusicNote to "Music",
+        Icons.Default.Star to "Star"
+    )
     var selectedIndex by remember { mutableIntStateOf(0) }
     SingleChoiceSegmentedButtonRow {
-        options.forEachIndexed { index, text ->
+        options.forEachIndexed { index, (imageVector, text) ->
             SegmentedButton(
                 selected = selectedIndex == index,
                 onClick = { selectedIndex = index },
@@ -27,9 +38,17 @@ private fun SingleChoiceSegmentedButtonRowPreview() {
                     index = index,
                     count = options.size,
                 ),
-            ) {
-                Text(text = text)
-            }
+                icon = {
+                    Icon(
+                        imageVector = imageVector,
+                        contentDescription = null,
+                        modifier = Modifier.size(SegmentedButtonDefaults.IconSize)
+                    )
+                },
+                label = {
+                    Text(text = text)
+                }
+            )
         }
     }
 }
