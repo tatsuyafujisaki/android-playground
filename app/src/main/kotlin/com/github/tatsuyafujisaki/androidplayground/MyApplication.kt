77580adc09c8b08877ec329be86472064e66af2a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-30 00:02:32 +0900

    Annotate MyApplication with @HiltAndroidApp

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/MyApplication.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/MyApplication.kt
@@ -1,5 +1,7 @@
 package com.github.tatsuyafujisaki.androidplayground
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyApplication : Application()
