549fd91960e5083bb6e115160352ff0c74027268
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-05-01 21:44:45 +0900

    Add MyRealmRepository

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/MyRealmPerson.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/MyRealmPerson.kt
@@ -1,24 +1,12 @@
 package com.github.tatsuyafujisaki.androidplayground.data
 
-import io.realm.kotlin.Realm
-import io.realm.kotlin.RealmConfiguration
 import io.realm.kotlin.types.RealmInstant
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
-import org.mongodb.kbson.ObjectId
 
 class MyRealmPerson : RealmObject {
     @PrimaryKey
-    var id: ObjectId = ObjectId()
     var name: String = ""
     var age: Int = 0
     var createdAt: RealmInstant = RealmInstant.now()
-}
-
-private fun main() {
-    val realm = Realm.open(
-        configuration = RealmConfiguration.create(
-            schema = setOf(MyRealmPerson::class)
-        )
-    )
 }
---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/domain/MyRealmRepository.kt
@@ -0,0 +1,28 @@
+package com.github.tatsuyafujisaki.androidplayground.domain
+
+import com.github.tatsuyafujisaki.androidplayground.data.MyRealmPerson
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.UpdatePolicy
+import io.realm.kotlin.ext.query
+
+class MyRealmRepository {
+    private val realm = Realm.open(
+        RealmConfiguration.Builder(schema = setOf(MyRealmPerson::class)).schemaVersion(1).build(),
+    )
+
+    fun getOrNull(name: String) =
+        realm.query<MyRealmPerson>("name == $0", name).find().firstOrNull()
+
+    fun upsert(name: String, age: Int) {
+        realm.writeBlocking {
+            copyToRealm(
+                instance = MyRealmPerson().apply {
+                    this.name = name
+                    this.age = age
+                },
+                updatePolicy = UpdatePolicy.ALL,
+            )
+        }
+    }
+}
