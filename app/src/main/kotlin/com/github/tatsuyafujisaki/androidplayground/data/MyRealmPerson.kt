b1891d746cf9292816f9c444750b455b58584644
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-05-01 21:32:58 +0900

    Update MyRealmPerson.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/MyRealmPerson.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/MyRealmPerson.kt
@@ -1,10 +1,10 @@
 package com.github.tatsuyafujisaki.androidplayground.data
 
-import io.realm.kotlin.internal.toDuration
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
 import io.realm.kotlin.types.RealmInstant
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
-import kotlin.time.Duration
 import org.mongodb.kbson.ObjectId
 
 class MyRealmPerson : RealmObject {
@@ -12,5 +12,13 @@ class MyRealmPerson : RealmObject {
     var id: ObjectId = ObjectId()
     var name: String = ""
     var age: Int = 0
-    var createdAt: Duration = RealmInstant.now().toDuration()
+    var createdAt: RealmInstant = RealmInstant.now()
+}
+
+private fun main() {
+    val realm = Realm.open(
+        configuration = RealmConfiguration.create(
+            schema = setOf(MyRealmPerson::class)
+        )
+    )
 }
