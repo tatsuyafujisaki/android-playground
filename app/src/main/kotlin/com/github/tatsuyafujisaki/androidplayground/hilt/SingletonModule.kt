8768b86f7acc6086005919ca28ab96122282ba85
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-05-08 06:52:27 +0900

    Rename ActivityModule.kt to SingletonModule.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/hilt/SingletonModule.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/hilt/SingletonModule.kt
@@ -3,12 +3,12 @@ package com.github.tatsuyafujisaki.androidplayground.hilt
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 
 @Module
-@InstallIn(ActivityComponent::class)
-object ActivityModule {
+@InstallIn(SingletonComponent::class)
+object SingletonModule {
     @Provides
     fun provideJson() = Json {
         ignoreUnknownKeys = true
