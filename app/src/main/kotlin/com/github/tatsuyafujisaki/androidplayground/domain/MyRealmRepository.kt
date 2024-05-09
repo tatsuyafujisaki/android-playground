74e57ea94c4b92a1c119d1f507896f3c212c22d6
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-05-09 16:21:30 +0900

    Create MyRealmRepositoryImpl

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/domain/MyRealmRepository.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/domain/MyRealmRepository.kt
@@ -1,35 +1,7 @@
 package com.github.tatsuyafujisaki.androidplayground.domain
 
-import com.github.tatsuyafujisaki.androidplayground.data.MyRealmPerson
-import io.realm.kotlin.Realm
-import io.realm.kotlin.RealmConfiguration
-import io.realm.kotlin.UpdatePolicy
-import io.realm.kotlin.ext.query
-
-class MyRealmRepository {
-    private val realm = Realm.open(
-        RealmConfiguration.Builder(schema = setOf(MyRealmPerson::class))
-            .schemaVersion(schemaVersion = 1).build(),
-    )
-
-    fun getOrNull(name: String) =
-        realm.query<MyRealmPerson>("name == $0", name).find().firstOrNull()
-
-    suspend fun upsert(name: String, age: Int) {
-        realm.write {
-            copyToRealm(
-                instance = MyRealmPerson().apply {
-                    this.name = name
-                    this.age = age
-                },
-                updatePolicy = UpdatePolicy.ALL,
-            )
-        }
-    }
-
-    suspend fun delete(name: String) {
-        realm.write {
-            delete(realm.query<MyRealmPerson>("name == $0", name).find())
-        }
-    }
+interface MyRealmRepository {
+    fun getOrNull(name: String)
+    suspend fun upsert(name: String, age: Int)
+    suspend fun delete(name: String)
 }
---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/domain/MyRealmRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.github.tatsuyafujisaki.androidplayground.domain
+
+import com.github.tatsuyafujisaki.androidplayground.data.MyRealmPerson
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.UpdatePolicy
+import io.realm.kotlin.ext.query
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MyRealmRepositoryImpl @Inject constructor() {
+    private val realm = Realm.open(
+        RealmConfiguration.Builder(schema = setOf(MyRealmPerson::class))
+            .schemaVersion(schemaVersion = 1).build(),
+    )
+
+    fun getOrNull(name: String) =
+        realm.query<MyRealmPerson>("name == $0", name).find().firstOrNull()
+
+    suspend fun upsert(name: String, age: Int) {
+        realm.write {
+            copyToRealm(
+                instance = MyRealmPerson().apply {
+                    this.name = name
+                    this.age = age
+                },
+                updatePolicy = UpdatePolicy.ALL,
+            )
+        }
+    }
+
+    suspend fun delete(name: String) {
+        realm.write {
+            delete(realm.query<MyRealmPerson>("name == $0", name).find())
+        }
+    }
+}
