479b70a72482699f425aff8d35867b9d96bf60f7
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-04 17:48:48 +0900

    Create CircleSpacerExample.kt and RoundedRectangleSpacerExample.kt

---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/spacer/CircleSpacerExample.kt
@@ -0,0 +1,29 @@
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.spacer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+private fun CircleSpacerExample() {
+    Spacer(
+        modifier = Modifier
+            .clip(shape = CircleShape) // avoids painting outside the corners.
+            .background(color = Color.Magenta)
+            .border(
+                width = 4.dp,
+                color = Color.Cyan,
+                shape = CircleShape, // changes the shape of the border from a rectangle to a circle.
+            )
+            .size(size = 100.dp),
+    )
+}
---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/spacer/RoundedCornerSpacerExample.kt
@@ -0,0 +1,31 @@
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.spacer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+private fun RoundedRectangleSpacerExample() {
+    val shape = RoundedCornerShape(size = 36.dp)
+
+    Spacer(
+        modifier = Modifier
+            .clip(shape = shape) // avoids painting outside the corners.
+            .background(color = Color.Magenta)
+            .border(
+                width = 4.dp,
+                color = Color.Cyan,
+                shape = shape, // changes the shape of the border from a rectangle to a rounded rectangle.
+            )
+            .size(size = 100.dp),
+    )
+}
