f08008616814c75f6406832c127e6dbfc5e30df1
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-04 17:26:30 +0900

    Update CenterBoxExample.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/CenterBoxExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/CenterBoxExample.kt
@@ -4,33 +4,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun CenterBoxExample(
-    height: Dp = 100.dp,
-    background: Color = Color.White,
     onClick: (() -> Unit)? = null,
     content: @Composable BoxScope.() -> Unit,
 ) {
     Box(
-        modifier =
-        Modifier
-            .sizeIn(minWidth = 48.dp, minHeight = 48.dp)
-            .fillMaxWidth()
-            .height(height)
-            .background(background)
-            .clickable(enabled = onClick != null, onClick = onClick ?: {}),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Magenta)
+            .clickable(
+                enabled = onClick != null,
+                onClick = onClick ?: {},
+            ),
         contentAlignment = Alignment.Center,
         content = content,
     )
@@ -40,6 +34,6 @@ fun CenterBoxExample(
 @Composable
 private fun CenterBoxExamplePreview() {
     CenterBoxExample {
-        Text("Sample")
+        Text(text = "Sample")
     }
 }
