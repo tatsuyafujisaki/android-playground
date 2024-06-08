db4ed276703b9bd5da3b4b0fc3b1afb96eb0eb6e
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-08 09:03:42 +0900

    Update dividers

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/divider/HorizontalDividerExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/divider/HorizontalDividerExample.kt
@@ -1,34 +1,25 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.divider
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-
-@Composable
-fun HorizontalDividerExample(
-    color: Color,
-    thickness: Dp,
-) {
-    HorizontalDivider(
-        thickness = thickness,
-        color = color,
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 private fun HorizontalDividerPreview() {
-    Column {
-        Text("Hello")
-        HorizontalDividerExample(
-            color = Color.Yellow,
-            thickness = 10.dp,
+    Column(modifier = Modifier.width(IntrinsicSize.Min)) {
+        Text("🍎")
+        HorizontalDivider(
+            thickness = 8.dp,
+            color = Color.Red,
         )
-        Text("World")
+        Text("🍊")
     }
 }
--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/divider/VerticalDividerExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/divider/VerticalDividerExample.kt
@@ -2,44 +2,24 @@ package com.github.tatsuyafujisaki.androidplayground.ui.compose.divider
 
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-
-@Composable
-fun VerticalDividerExample(
-    width: Dp,
-    color: Color,
-) {
-    // Divider's "thickness" cannot be used for a vertical divider
-    // because "thickness" is always interpreted as the height.
-    // Moreover, the width of Divider is infinite unless specified.
-    HorizontalDivider(
-        modifier =
-        Modifier
-            .width(width)
-            .fillMaxHeight(),
-        color = color,
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 private fun VerticalDividerExamplePreview() {
-    Row(Modifier.height(IntrinsicSize.Min)) {
-        Text("Hello")
-        VerticalDividerExample(
-            width = 10.dp,
-            color = Color.Yellow,
+    Row(modifier = Modifier.height(IntrinsicSize.Min)) {
+        Text(text = "🍎")
+        VerticalDivider(
+            thickness = 8.dp,
+            color = Color.Red,
         )
-        Text("World")
+        Text(text = "🍊")
     }
 }
