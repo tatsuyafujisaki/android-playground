628a44c6a18c12bd20f5f6c22f7902d710a95771
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-10 09:38:07 +0900

    Update ImageExamples.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/ImageExamples.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/ImageExamples.kt
@@ -6,18 +6,31 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.github.tatsuyafujisaki.androidplayground.R
 
 @Preview(showBackground = true)
 @Composable
-private fun ImageExample() {
+private fun ImageExample1() {
     Image(
         imageVector = Icons.Default.Favorite,
         contentDescription = null,
         modifier = Modifier.size(size = 48.dp),
-        colorFilter = ColorFilter.tint(color = Red)
+        colorFilter = ColorFilter.tint(color = Color.Red)
+    )
+}
+
+@Preview
+@Composable
+private fun IconExample2() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_android_robot),
+        contentDescription = null,
+        modifier = Modifier.size(size = 48.dp),
+        colorFilter = ColorFilter.tint(color = Color.Green)
     )
 }
