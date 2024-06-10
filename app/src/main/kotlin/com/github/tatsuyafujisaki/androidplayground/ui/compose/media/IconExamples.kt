952f3b96cf7184e51d45476bb7953d501cba507a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-10 09:36:37 +0900

    Update IconExamples.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/IconExamples.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/IconExamples.kt
@@ -1,12 +1,15 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.media
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.tatsuyafujisaki.androidplayground.R
 
 @Preview
@@ -15,6 +18,7 @@ private fun IconExample1() {
     Icon(
         imageVector = Icons.Default.Favorite,
         contentDescription = null,
+        modifier = Modifier.size(size = 48.dp),
         tint = Color.Red,
     )
 }
@@ -25,6 +29,7 @@ private fun IconExample2() {
     Icon(
         painter = painterResource(id = R.drawable.ic_android_robot),
         contentDescription = null,
+        modifier = Modifier.size(size = 48.dp),
         tint = Color.Green,
     )
 }
