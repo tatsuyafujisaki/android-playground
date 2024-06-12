9114fb05661b492ae0f18047812ac9685599a52c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-12 18:59:36 +0900

    Update IconButtonExample.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/button/IconButtonExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/button/IconButtonExample.kt
@@ -5,30 +5,18 @@ import androidx.compose.material.icons.filled.Android
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-
-@Composable
-fun IconButtonExample(
-    imageVector: ImageVector,
-    modifier: Modifier = Modifier,
-    onClick: (() -> Unit)? = null,
-) {
-    IconButton(
-        onClick = onClick ?: {},
-        modifier = modifier,
-        enabled = onClick != null,
-    ) {
-        Icon(
-            imageVector = imageVector,
-            contentDescription = null,
-        )
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-private fun IconButtonExamplePreview() {
-    IconButtonExample(imageVector = Icons.Default.Android)
+fun IconButtonExample(onClick: (() -> Unit)? = null) {
+    IconButton(
+        onClick = onClick ?: {},
+        enabled = onClick != null,
+    ) {
+        Icon(
+            imageVector = Icons.Default.Android,
+            contentDescription = null,
+        )
+    }
 }
