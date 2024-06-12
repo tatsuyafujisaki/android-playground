b0f9c6e53cff9459f846af0dca67f99ed7824a26
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-12 19:20:12 +0900

    Update IconButtons

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/iconbutton/IconButtonExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/iconbutton/IconButtonExample.kt
@@ -1,19 +1,23 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.media.iconbutton
 
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Android
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import com.github.tatsuyafujisaki.androidplayground.ui.compose.preview.NullableLambdaProvider
 
 @Composable
 private fun IconButtonExample(onClick: (() -> Unit)?) {
     IconButton(
         onClick = onClick ?: {},
+        modifier = Modifier.sizeIn(minWidth = 48.dp, minHeight = 48.dp),
         enabled = onClick != null,
     ) {
         Icon(
--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/iconbutton/IconToggleButtonExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/iconbutton/IconToggleButtonExample.kt
@@ -1,5 +1,6 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.media.iconbutton
 
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.HeartBroken
@@ -10,8 +11,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -20,6 +23,7 @@ private fun IconToggleButtonExample() {
 
     IconToggleButton(
         checked = checked,
+        modifier = Modifier.sizeIn(minWidth = 48.dp, minHeight = 48.dp),
         onCheckedChange = { checked = it },
     ) {
         Icon(
