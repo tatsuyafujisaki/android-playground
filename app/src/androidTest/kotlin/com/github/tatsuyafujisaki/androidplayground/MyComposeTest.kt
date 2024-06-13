a99aebfd4831ea4b486e5a9cc19c9d8fcc87c0d1
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-14 08:24:40 +0900

    Update MyComposeTest.kt

--- app/src/androidTest/kotlin/com/github/tatsuyafujisaki/androidplayground/MyComposeTest.kt
+++ app/src/androidTest/kotlin/com/github/tatsuyafujisaki/androidplayground/MyComposeTest.kt
@@ -1,7 +1,7 @@
 package com.github.tatsuyafujisaki.androidplayground
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
