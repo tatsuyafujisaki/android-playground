c3620bed6ce64637ea5fbf4b9bcd6791d807cf51
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-13 19:17:43 +0900

    Update TextButton44.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/button/TextButton44.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/button/TextButton44.kt
@@ -1,11 +1,9 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.button
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,22 +14,17 @@ private fun TextButton44(
     text: String,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier
-            .sizeIn(minWidth = 44.dp, minHeight = 44.dp)
-            .clickable(onClick = onClick),
-        contentAlignment = Alignment.Center,
+    TextButton(
+        onClick = onClick,
+        modifier = modifier.sizeIn(minWidth = 44.dp, minHeight = 44.dp)
     ) {
-        Text(
-            text = text,
-            modifier = modifier,
-        )
+        Text(text = text)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun TextButton48Preview() {
+private fun TextButton44Preview() {
     TextButton44(
         onClick = {},
         text = "あ"
