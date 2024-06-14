0b36415c7b64e034d980c24b707758135b360dd0
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-14 10:28:30 +0900

    Create StartCenterEndTextInBoxExample

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/align/StartCenterEndTextInBoxExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/align/StartCenterEndTextInBoxExample.kt
@@ -1,44 +1,41 @@
-package com.github.tatsuyafujisaki.androidplayground.ui.compose.row
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.box.align
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
-private fun CenterAndEndInRowExample() {
+private fun StartCenterEndTextInBoxExample() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(height = 48.dp),
-        contentAlignment = Alignment.Center,
     ) {
         Text(
             text = "🍎",
-            textAlign = TextAlign.Center,
+            modifier = Modifier.align(alignment = Alignment.CenterStart),
             overflow = TextOverflow.Ellipsis,
             maxLines = 1,
         )
-        IconButton(
-            onClick = {},
+        Text(
+            text = "🍊",
+            modifier = Modifier.align(alignment = Alignment.Center),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
+        Text(
+            text = "🍏",
             modifier = Modifier.align(alignment = Alignment.CenterEnd),
-        ) {
-            Icon(
-                imageVector = Icons.Default.Close,
-                contentDescription = null,
-            )
-        }
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
     }
 }
