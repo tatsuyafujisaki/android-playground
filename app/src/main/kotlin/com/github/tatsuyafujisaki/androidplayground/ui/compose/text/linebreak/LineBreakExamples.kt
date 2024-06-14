188fad53eb115e1ea746d5dfe9fa8d92bde524f0
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-14 17:25:40 +0900

    Create LineBreakExamples.kt

---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/text/linebreak/LineBreakExamples.kt
@@ -0,0 +1,79 @@
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.text.linebreak
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true)
+@Composable
+fun LineBreakExamples() {
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = GridCells.Fixed(count = 4),
+        verticalArrangement = Arrangement.spacedBy(2.dp),
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+    ) {
+        item {
+            Text(text = "LineBreak.Unspecified")
+        }
+        item {
+            Text(text = "LineBreak.Simple")
+        }
+        item {
+            Text(text = "LineBreak.Heading")
+        }
+        item {
+            Text(text = "LineBreak.Paragraph")
+        }
+        repeat(100) {
+            val texts = listOf(
+                generateText(s = "a", charCountPerWord = it + 1),
+                generateText(s = "あ", charCountPerWord = it + 1)
+            )
+            texts.forEach {
+                item {
+                    Text(
+                        text = it,
+                        modifier = Modifier.background(color = Color.Cyan),
+                        style = TextStyle(lineBreak = LineBreak.Unspecified),
+                    )
+                }
+                item {
+                    Text(
+                        text = it,
+                        modifier = Modifier.background(color = Color.Magenta),
+                        style = TextStyle(lineBreak = LineBreak.Simple)
+                    )
+                }
+                item {
+                    Text(
+                        text = it,
+                        modifier = Modifier.background(color = Color.Cyan),
+                        style = TextStyle(lineBreak = LineBreak.Heading)
+                    )
+                }
+                item {
+                    Text(
+                        text = it,
+                        modifier = Modifier.background(color = Color.Magenta),
+                        style = TextStyle(lineBreak = LineBreak.Paragraph)
+                    )
+                }
+            }
+        }
+    }
+}
+
+private fun generateText(s: String, charCountPerWord: Int) =
+    List(size = 10) { s.repeat(charCountPerWord) }
+        .joinToString(separator = " ")
