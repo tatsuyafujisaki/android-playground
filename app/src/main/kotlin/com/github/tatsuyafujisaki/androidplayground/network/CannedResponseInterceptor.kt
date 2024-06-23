fb0d1adb85befd05b5286dec27afcc79822290bd
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-23 18:06:10 +0900

    Create CannedResponseInterceptor.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/network/CannedResponseInterceptor.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/network/CannedResponseInterceptor.kt
@@ -6,39 +6,31 @@ import okhttp3.Protocol
 import okhttp3.Response
 import okhttp3.ResponseBody.Companion.toResponseBody
 
-// Consider using OkHttp's built-in cache, instead.
-// https://square.github.io/okhttp/recipes/#response-caching-kt-java
-class CacheInterceptor(
+class CannedResponseInterceptor(
     private val cache: Set<Triple<String, String, LocalDateTime>>,
     private val upsert: (String, String) -> Unit,
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        val url = request.url.toString()
-        val cache =
-            cache.filter { (cachedUrl, responseJson, createdAt) ->
-                cachedUrl == url && isLessThan1HoursOld(createdAt)
-            }.map { (_, responseJson, _) ->
-                responseJson
-            }.firstOrNull()
 
-        return if (cache != null) {
-            Response.Builder()
+        fun shouldReturnCannedResponse(url: String): Boolean {
+            return url.contains("/foo/bar?baz=qux")
+        }
+
+        return if (shouldReturnCannedResponse(request.url.toString())) {
+            val cannedResponse = """
+                { "statusCode":"500" }
+                """.trimIndent()
+
+            Response
+                .Builder()
                 .request(request)
                 .protocol(Protocol.HTTP_2) // You must specify a protocol or the "java.lang.IllegalStateException: protocol == null" error will be thrown.
                 .code(200) // You must specify a code or the "java.lang.IllegalStateException: code < 0: -1" error will be thrown.
                 .message("") // You must specify a message or the "java.lang.IllegalStateException: message == null" will be thrown.
-                .body(cache.toResponseBody())
-                .build()
+                .body(cannedResponse.toResponseBody()).build()
         } else {
-            chain.proceed(request).apply {
-                if (isSuccessful) {
-                    upsert(url, peekBody(Long.MAX_VALUE).string())
-                }
-            }
+            chain.proceed(request)
         }
     }
-
-    private fun isLessThan1HoursOld(localDateTime: LocalDateTime) =
-        localDateTime.isAfter(LocalDateTime.now().minusHours(1))
 }
