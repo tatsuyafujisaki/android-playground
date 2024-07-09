b14014868b69008302e45bc843f4f8bd0ffdc923
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-07-09 19:43:01 +0900

    Update PullToRefreshBoxExample.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/pulltorefresh/PullToRefreshBoxExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/pulltorefresh/PullToRefreshBoxExample.kt
@@ -1,6 +1,5 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.box.pulltorefresh
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
@@ -14,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
@@ -22,6 +20,9 @@ import com.github.tatsuyafujisaki.androidplayground.util.RandomImage
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+/**
+ * https://developer.android.com/reference/kotlin/androidx/compose/material3/pulltorefresh/package-summary#PullToRefreshBox(kotlin.Boolean,kotlin.Function0,androidx.compose.ui.Modifier,androidx.compose.material3.pulltorefresh.PullToRefreshState,androidx.compose.ui.Alignment,kotlin.Function1,kotlin.Function1)
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
@@ -50,9 +51,7 @@ private fun PullToRefreshBoxExample() {
             AsyncImage(
                 model = imageUrl,
                 contentDescription = null,
-                modifier = Modifier
-                    .background(Color.Red)
-                    .fillMaxSize(),
+                modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.Crop,
             )
         }
