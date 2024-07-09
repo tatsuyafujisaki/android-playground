4b9fbb05b9e87a01e62ad0546fce8f7ff756fc46
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-07-09 18:09:12 +0900

    Delete StatefulPullRefreshBox

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/pulltorefresh/Material2PullRefreshBox.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/box/pulltorefresh/Material2PullRefreshBox.kt
@@ -1,25 +1,23 @@
-package com.github.tatsuyafujisaki.androidplayground.ui.compose.box
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.box.pulltorefresh
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.PullRefreshState
 import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun StatefulPullRefreshBox(
+fun Material2PullRefreshBox(
     refreshing: Boolean,
-    onRefresh: () -> Unit,
+    state: PullRefreshState,
     modifier: Modifier = Modifier,
     content: @Composable BoxScope.() -> Unit,
 ) {
-    val state = rememberPullRefreshState(refreshing, onRefresh)
-
     Box(modifier = modifier.pullRefresh(state)) {
         content()
         PullRefreshIndicator(
