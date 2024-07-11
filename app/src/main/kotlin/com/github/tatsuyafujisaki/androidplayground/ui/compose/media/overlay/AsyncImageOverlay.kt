b1c6b1f44ee96c61adac2bb93c3ab7c49ce94daa
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-07-11 13:39:31 +0900

    Create AsyncImageOverlay.kt and ImageOverlay.kt

---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/overlay/AsyncImageOverlay.kt
@@ -0,0 +1,46 @@
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.media.overlay
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import com.github.tatsuyafujisaki.androidplayground.R
+import com.github.tatsuyafujisaki.androidplayground.util.RandomImage
+
+@Composable
+fun AsyncImageOverlay(
+    url: String,
+    @DrawableRes mask: Int,
+    maskAlpha: Float,
+    modifier: Modifier = Modifier,
+) {
+    AsyncImage(
+        model = url,
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = ContentScale.Crop,
+    )
+    Image(
+        painter = painterResource(id = mask),
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = ContentScale.Crop,
+        alpha = maskAlpha,
+    )
+}
+
+@Preview
+@Composable
+private fun AsyncImageOverlayPreview() {
+    AsyncImageOverlay(
+        url = RandomImage.getUrl(),
+        mask = R.drawable.solid_yellow,
+        maskAlpha = 0.5f,
+        modifier = Modifier.aspectRatio(ratio = 1f / 1f),
+    )
+}
---
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/overlay/ImageOverlay.kt
@@ -0,0 +1,45 @@
+package com.github.tatsuyafujisaki.androidplayground.ui.compose.media.overlay
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.tatsuyafujisaki.androidplayground.R
+
+@Composable
+fun ImageOverlay(
+    @DrawableRes main: Int,
+    @DrawableRes mask: Int,
+    maskAlpha: Float,
+    modifier: Modifier = Modifier,
+) {
+    Image(
+        painter = painterResource(id = main),
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = ContentScale.Crop,
+        alpha = maskAlpha,
+    )
+    Image(
+        painter = painterResource(id = mask),
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = ContentScale.Crop,
+        alpha = maskAlpha,
+    )
+}
+
+@Preview
+@Composable
+private fun ImageOverlayPreview() {
+    ImageOverlay(
+        main = R.drawable.ic_android_robot,
+        mask = R.drawable.solid_yellow,
+        maskAlpha = 0.5f,
+        modifier = Modifier.aspectRatio(ratio = 1f / 1f),
+    )
+}
