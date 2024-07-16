9ff24d018e6a117b3f2bfa86176856bcd72b3ff1
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-07-16 12:42:24 +0900

    Revert "Update StartAndEndRowExample.kt"
    This reverts commit f9cab1bd5ea559ae0f69031fbcdeb6e7223b5036.

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/row/StartAndEndRowExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/row/StartAndEndRowExample.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -21,7 +22,15 @@ private fun StartAndEndRowExample() {
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Text(text = "🍎")
-        Text(text = "🍊")
+        Text(
+            text = "🍎",
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
+        Text(
+            text = "🍊",
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
     }
 }
