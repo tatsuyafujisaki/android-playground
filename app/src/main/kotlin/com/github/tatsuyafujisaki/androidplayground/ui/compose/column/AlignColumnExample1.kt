0d5de954f2615d4ab38bffea1d69422bc4fb7f2d
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-07-17 08:32:47 +0900

    Update AlignColumnExample1.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/column/AlignColumnExample1.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/column/AlignColumnExample1.kt
@@ -4,23 +4,21 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Preview(showBackground = true)
 @Composable
-fun AlignColumnExample(
+fun AlignColumnExample1(
     onClick: (() -> Unit)? = null,
 ) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .height(height = 100.dp)
             .clickable(enabled = onClick != null, onClick = onClick ?: {}),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -28,5 +26,22 @@ fun AlignColumnExample(
         Text(text = "🍎", modifier = Modifier.align(Alignment.Start))
         Text(text = "🍏")
         Text(text = "🍊", modifier = Modifier.align(Alignment.End))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlignColumnExample2(
+    onClick: (() -> Unit)? = null,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(enabled = onClick != null, onClick = onClick ?: {}),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = "🍎", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Start)
+        Text(text = "🍏", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+        Text(text = "🍊", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.End)
     }
 }
