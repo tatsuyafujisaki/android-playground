741cd5cf8ddc60ddd855e3cd559fbd7a8dd383e5
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-07-23 14:03:01 +0900

    Update HorizontalPagerExample.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/pager/HorizontalPagerExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/pager/HorizontalPagerExample.kt
@@ -1,15 +1,26 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.compose.pager
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.github.tatsuyafujisaki.androidplayground.util.RandomImage
 
@@ -21,20 +32,37 @@ fun HorizontalPagerExample() {
     val pagerState = rememberPagerState(pageCount = { urls.size })
     val uriHandler = LocalUriHandler.current
 
-    HorizontalPager(
-        state = pagerState,
-        modifier = Modifier.fillMaxSize(),
-        beyondViewportPageCount = pagerState.pageCount - 1,
-    ) { page ->
-        val url = urls[page]
-
-        AsyncImage(
-            model = url,
-            contentDescription = null,
+    Box(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize(),
+            beyondViewportPageCount = pagerState.pageCount - 1,
+        ) {
+            val url = urls[it]
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable { uriHandler.openUri(uri = url) },
+                contentScale = ContentScale.Crop,
+            )
+        }
+        Row(
             modifier = Modifier
-                .fillMaxSize()
-                .clickable { uriHandler.openUri(uri = url) },
-            contentScale = ContentScale.Crop,
-        )
+                .align(alignment = Alignment.BottomCenter)
+                .padding(bottom = 40.dp),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            repeat(times = pagerState.pageCount) {
+                Box(
+                    modifier = Modifier
+                        .padding(all = 4.dp)
+                        .clip(shape = CircleShape)
+                        .background(color = if (pagerState.currentPage == it) Color.DarkGray else Color.LightGray)
+                        .size(size = 16.dp)
+                )
+            }
+        }
     }
 }
