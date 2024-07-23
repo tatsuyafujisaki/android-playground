d50b5e7fd4cf18d9b95a9e86f6a52ce22a4b0e5e
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-07-23 15:29:43 +0900

    Update HorizontalPagerExample.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/pager/HorizontalPagerExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/pager/HorizontalPagerExample.kt
@@ -3,12 +3,15 @@ package com.github.tatsuyafujisaki.androidplayground.ui.compose.pager
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
@@ -20,8 +23,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.github.tatsuyafujisaki.androidplayground.util.RandomImage
@@ -31,6 +36,7 @@ private val URLS = List(size = 3) { RandomImage.getUrl() }
 @Preview
 @Composable
 fun HorizontalPagerExample() {
+    val screenWidth = LocalConfiguration.current.screenWidthDp
     val pagerState = rememberPagerState(pageCount = { URLS.size })
     val uriHandler = LocalUriHandler.current
 
@@ -57,18 +63,39 @@ fun HorizontalPagerExample() {
         }
         Row(
             modifier = Modifier
-                .padding(bottom = 40.dp)
+                .padding(bottom = 8.dp)
                 .align(alignment = Alignment.BottomCenter),
+            horizontalArrangement = Arrangement.spacedBy(space = 4.dp)
         ) {
+            val singleIndicatorWidth = screenWidth * 0.64 / pagerState.pageCount
+
             repeat(times = pagerState.pageCount) {
-                Spacer(
-                    modifier = Modifier
-                        .padding(all = 4.dp)
-                        .clip(shape = CircleShape)
-                        .background(color = if (it == pagerState.currentPage) Color.LightGray else Color.DarkGray)
-                        .size(size = 16.dp)
+                LinearIndicator(
+                    width = singleIndicatorWidth.dp,
+                    currentPage = it == pagerState.currentPage
                 )
+                // CircularIndicator(currentPage = it == pagerState.currentPage)
             }
         }
     }
+}
+
+@Composable
+private fun CircularIndicator(currentPage: Boolean) {
+    Spacer(
+        modifier = Modifier
+            .clip(shape = CircleShape)
+            .background(color = if (currentPage) Color.White else Color.White.copy(alpha = 0.5f))
+            .size(size = 16.dp)
+    )
+}
+
+@Composable
+private fun LinearIndicator(width: Dp, currentPage: Boolean) {
+    Spacer(
+        modifier = Modifier
+            .background(color = if (currentPage) Color.White else Color.White.copy(alpha = 0.5f))
+            .width(width = width)
+            .height(height = 2.dp)
+    )
 }
