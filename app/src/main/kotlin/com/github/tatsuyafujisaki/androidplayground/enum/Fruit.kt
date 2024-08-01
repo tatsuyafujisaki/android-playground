060d2fa9960a96617c45d07a78370ae5ab85ccfb
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-08-02 08:34:36 +0900

    Update Fruit.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/enum/Fruit.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/enum/Fruit.kt
@@ -4,7 +4,7 @@ import androidx.annotation.StringRes
 import com.github.tatsuyafujisaki.androidplayground.R
 
 enum class Fruit(
-    @StringRes val resId: Int,
+    @StringRes val id: Int,
 ) {
     APPLE(R.string.apple),
     ORANGE(R.string.orange),
