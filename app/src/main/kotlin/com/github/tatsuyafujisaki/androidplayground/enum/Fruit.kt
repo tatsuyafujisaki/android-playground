ec247709d8716af18e9f70b35a1152740efe1351
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-08-04 09:37:26 +0900

    Use emoji for apple and orange

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/enum/Fruit.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/enum/Fruit.kt
@@ -6,6 +6,6 @@ import com.github.tatsuyafujisaki.androidplayground.R
 enum class Fruit(
     @StringRes val id: Int,
 ) {
-    APPLE(R.string.apple),
-    ORANGE(R.string.orange),
+    APPLE(id = R.string.apple),
+    ORANGE(id = R.string.orange),
 }
