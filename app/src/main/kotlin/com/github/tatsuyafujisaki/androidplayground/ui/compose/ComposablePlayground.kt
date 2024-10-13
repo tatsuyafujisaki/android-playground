adc8208c8274760c99706f898d3f01b2a6f16006
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-10-13 10:46:33 +0900

    Update ComposablePlayground.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/ComposablePlayground.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/ComposablePlayground.kt
@@ -3,6 +3,7 @@ package com.github.tatsuyafujisaki.androidplayground.ui.compose
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
+@Suppress("EmptyMethod")
 @Preview
 @Composable
 fun ComposablePlayground() {
