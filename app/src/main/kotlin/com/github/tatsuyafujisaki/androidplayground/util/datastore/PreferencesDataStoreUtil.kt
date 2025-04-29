b1e4fc75378e9dd13682b735085b4ab05e02321e
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-04-30 06:00:16 +0900

    Rename preferencesDataStore to dataStore

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/datastore/PreferencesDataStoreUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/datastore/PreferencesDataStoreUtil.kt
@@ -8,27 +8,27 @@ import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
-val Context.preferencesDataStore: DataStore<Preferences> by preferencesDataStore("")
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore("")
 
 object PreferencesDataStoreUtil {
     suspend fun <T> get(
         context: Context,
         key: Preferences.Key<T>,
         defaultValue: T,
-    ) = context.preferencesDataStore.data.map { it[key] ?: defaultValue }.first()
+    ) = context.dataStore.data.map { it[key] ?: defaultValue }.first()
 
     suspend fun <T> set(
         context: Context,
         key: Preferences.Key<T>,
         value: T,
     ) = runCatching {
-        context.preferencesDataStore.edit { it[key] = value }
+        context.dataStore.edit { it[key] = value }
     }.isSuccess
 
     suspend fun <T> remove(
         context: Context,
         key: Preferences.Key<T>,
     ) = runCatching {
-        context.preferencesDataStore.edit { it.remove(key) }
+        context.dataStore.edit { it.remove(key) }
     }.isSuccess
 }
