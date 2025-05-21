9a4c792319d223d799465d15fb900d79b941c09f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-05-21 13:31:14 +0900

    Define Modifier.safeClickable

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/ModifierExtensions.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/media/ModifierExtensions.kt
@@ -2,11 +2,17 @@ package com.github.tatsuyafujisaki.androidplayground.ui.compose.media
 
 import android.util.Log
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -33,9 +39,24 @@ fun Modifier.fillScreenWidth() = width(width = LocalConfiguration.current.screen
 @Composable
 fun Modifier.fillScreenHeight() = height(height = LocalConfiguration.current.screenHeightDp.dp)
 
-@Composable
 fun Modifier.logSizeChanged() = onSizeChanged {
     Log.d("👀", "onSizeChanged > size: $it[dp]")
     Log.d("👀", "onSizeChanged > width: ${it.width.dp}")
     Log.d("👀", "onSizeChanged > height: ${it.height.dp}")
+}
+
+@Composable
+fun Modifier.safeClickable(
+    delayMillis: Long = 1_000,
+    onClick: () -> Unit,
+): Modifier = composed {
+    var lastClickTime by remember { mutableLongStateOf(0L) }
+
+    clickable {
+        val now = System.currentTimeMillis()
+        if (now - lastClickTime >= delayMillis) {
+            onClick()
+            lastClickTime = now
+        }
+    }
 }
