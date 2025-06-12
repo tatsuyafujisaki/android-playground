43e3c513347e5f02e049eed4abd5414247260e8a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-06-12 20:09:23 +0900

    Add StrictMode

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/MyApplication.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/MyApplication.kt
@@ -1,7 +1,24 @@
 package com.github.tatsuyafujisaki.androidplayground
 
 import android.app.Application
+import android.os.Build
+import android.os.StrictMode
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import java.util.concurrent.Executors
 
 @HiltAndroidApp
-class MyApplication : Application()
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // https://developer.android.com/guide/app-compatibility/restrictions-non-sdk-interfaces#test-strictmode-api
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder().detectNonSdkApiUsage().penaltyLog()
+                    .penaltyListener(Executors.newSingleThreadExecutor()) {
+                        Log.e("StrictMode", "Non-SDK interface: $it")
+                    }.build()
+            )
+        }
+    }
+}
