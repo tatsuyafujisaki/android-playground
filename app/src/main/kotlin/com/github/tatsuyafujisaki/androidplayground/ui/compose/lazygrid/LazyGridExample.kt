ab38ecf37c7bbf12e5f62b4f04af2ebc5e1f3ce3
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-07-15 00:21:39 +0900

    Update LazyGridExample.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/lazygrid/LazyGridExample.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/lazygrid/LazyGridExample.kt
@@ -3,15 +3,19 @@ package com.github.tatsuyafujisaki.androidplayground.ui.compose.lazygrid
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun LazyGridExample(modifier: Modifier = Modifier) {
-    val lazyListState = rememberLazyListState()
+    val lazyListState = rememberLazyGridState()
 
-    LazyVerticalGrid(columns = GridCells.Fixed(count = 3)) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(count = 3),
+        modifier = modifier,
+        state = lazyListState
+    ) {
         item(span = { GridItemSpan(maxCurrentLineSpan) }) {
         }
     }
