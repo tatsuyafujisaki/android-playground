fba843c7a10e4ccf01d6f64ad6618973476f028f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-08-02 06:24:44 +0900

    Add @Suppress("unused") to FibonacciTest class

--- app/src/test/kotlin/com/github/tatsuyafujisaki/androidplayground/FibonacciTest.kt
+++ app/src/test/kotlin/com/github/tatsuyafujisaki/androidplayground/FibonacciTest.kt
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
 
+@Suppress("unused")
 @RunWith(Parameterized::class)
 class FibonacciTest(private val expected: Int, private val n: Int) {
     @Test
