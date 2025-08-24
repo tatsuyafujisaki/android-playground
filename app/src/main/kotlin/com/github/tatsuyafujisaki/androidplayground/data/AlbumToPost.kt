adb9c766b3b6d965f2fbe84b70cd90d64af78ba0
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-08-25 06:46:18 +0900

    Add @Suppress("unused") to AlbumToPost data class

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/AlbumToPost.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/AlbumToPost.kt
@@ -2,6 +2,7 @@ package com.github.tatsuyafujisaki.androidplayground.data
 
 import kotlinx.serialization.Serializable
 
+@Suppress("unused")
 @Serializable
 data class AlbumToPost(
     val title: String,
