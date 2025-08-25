9e40d27b4beebd5c7492fc37a6cdaefdd13f7111
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-08-26 06:24:45 +0900

    Add @Suppress("unused") to SpacerHeight composable

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/spacer/DebuggingTools.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/spacer/DebuggingTools.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+@Suppress("unused")
 @Composable
 private fun SpacerHeight(
     modifier: Modifier = Modifier,
