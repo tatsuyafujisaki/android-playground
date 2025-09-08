8259116df0715a6e3b5096e96f0b6a51e50bc099
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-09-09 07:26:34 +0900

    Add @Suppress("unused") to BooleanProvider

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/preview/BooleanProvider.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/preview/BooleanProvider.kt
@@ -2,6 +2,7 @@ package com.github.tatsuyafujisaki.androidplayground.ui.compose.preview
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 
+@Suppress("unused")
 class BooleanProvider : PreviewParameterProvider<Boolean> {
     override val values = sequenceOf(true, false)
 }
