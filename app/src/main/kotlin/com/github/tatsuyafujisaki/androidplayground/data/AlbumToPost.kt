dad35103c25129db3e48b523bbc57dbbded42a6b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-01-09 13:35:42 +0900

    Remove unnecessary @Suppress("unused") annotations

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/AlbumToPost.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/data/AlbumToPost.kt
@@ -2,7 +2,6 @@ package com.github.tatsuyafujisaki.androidplayground.data
 
 import kotlinx.serialization.Serializable
 
-@Suppress("unused")
 @Serializable
 data class AlbumToPost(
     val title: String,
--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/preview/BooleanProvider.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/compose/preview/BooleanProvider.kt
@@ -2,7 +2,6 @@ package com.github.tatsuyafujisaki.androidplayground.ui.compose.preview
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 
-@Suppress("unused")
 class BooleanProvider : PreviewParameterProvider<Boolean> {
     override val values = sequenceOf(true, false)
 }
