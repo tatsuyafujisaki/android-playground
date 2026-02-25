ac2a6d863296b05081869ddf97f15b3afa78739b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-02-26 08:35:31 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
@@ -10,11 +10,8 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "+"
 }
 
-// https://developer.android.com/build/remote-repositories
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
