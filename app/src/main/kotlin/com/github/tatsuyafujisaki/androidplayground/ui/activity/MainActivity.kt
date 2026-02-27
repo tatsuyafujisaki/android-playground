dbeaa7b381f27c4ce64223b442adee07b6b91307
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-02-28 00:39:40 +0900

    Rewrite MainActivity with Jetpack Compose

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/activity/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.activity
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.github.tatsuyafujisaki.androidplayground.R
 import com.github.tatsuyafujisaki.androidplayground.data.MyRemoteConfig
-import com.github.tatsuyafujisaki.androidplayground.databinding.ActivityMainBinding
 import com.github.tatsuyafujisaki.androidplayground.network.RetrofitClient
+import com.github.tatsuyafujisaki.androidplayground.ui.compose.screen.MainScreen
+import com.github.tatsuyafujisaki.androidplayground.ui.compose.screen.SecondScreen
+import com.github.tatsuyafujisaki.androidplayground.ui.compose.screen.ThirdScreen
 import com.github.tatsuyafujisaki.androidplayground.ui.viewmodel.MyActivityViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.remoteconfig.get
@@ -26,32 +47,87 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var json: Json
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var navHostFragment: NavHostFragment
     private val viewModel: MyActivityViewModel by viewModels()
-    private val navController get() = navHostFragment.navController
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            val navController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
 
-        navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val items = listOf(
+                Screen.Main,
+                Screen.Second,
+                Screen.Third
+            )
 
-        binding.bottomNavigationView.setupWithNavController(navController)
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.main_fragment, R.id.second_fragment, R.id.third_fragment),
-        )
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
-        setSupportActionBar(binding.toolbar)
+            MaterialTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = when (currentDestination?.route) {
+                                        Screen.Main.route -> stringResource(R.string.main)
+                                        Screen.Second.route -> stringResource(R.string.second)
+                                        Screen.Third.route -> stringResource(R.string.third)
+                                        else -> stringResource(R.string.app_name)
+                                    }
+                                )
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                titleContentColor = MaterialTheme.colorScheme.onPrimary
+                            )
+                        )
+                    },
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEach { screen ->
+                                NavigationBarItem(
+                                    icon = {
+                                        Icon(
+                                            painter = painterResource(id = screen.iconId),
+                                            contentDescription = null
+                                        )
+                                    },
+                                    label = { Text(stringResource(screen.resourceId)) },
+                                    selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                    onClick = {
+                                        navController.navigate(screen.route) {
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            launchSingleTop = true
+                                            restoreState = true
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
+                ) { innerPadding ->
+                    NavHost(
+                        navController,
+                        startDestination = Screen.Main.route,
+                        Modifier.padding(innerPadding)
+                    ) {
+                        composable(Screen.Main.route) {
+                            MainScreen {
+                                navController.navigate(Screen.Third.route)
+                            }
+                        }
+                        composable(Screen.Second.route) { SecondScreen() }
+                        composable(Screen.Third.route) { ThirdScreen() }
+                    }
+                }
+            }
+        }
 
         lifecycleScope.launch {
             val service = RetrofitClient.createJsonPlaceholderService()
-//            println(service.getAlbums())
-//            println(service.getAlbum(id = 1))
-//            println(service.postAlbum(AlbumToPost(title = "🍎")))
             service.deleteAlbum(id = 1)
         }
 
@@ -76,5 +152,11 @@ class MainActivity : AppCompatActivity() {
                 println(myRemoteConfig)
             }
         }
+    }
+
+    sealed class Screen(val route: String, val resourceId: Int, val iconId: Int) {
+        object Main : Screen("main", R.string.main, R.drawable.baseline_home_24)
+        object Second : Screen("second", R.string.second, R.drawable.baseline_explore_24)
+        object Third : Screen("third", R.string.third, R.drawable.baseline_account_circle_24)
     }
 }
