90e247eb295b87fcec215a2514e5eff3d86d59ac
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-03-04 10:03:25 +0900

    Update MainActivity.kt

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -13,8 +14,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
