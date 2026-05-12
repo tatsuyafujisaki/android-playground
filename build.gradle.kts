5cf44a4a3810e41acc49cd977213a0cac0d28c18
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-05-12 09:18:53 +0900

    Reorder Crashlytics and Google Services plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.firebase.appdistribution) apply false
-    alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.protobuf) apply false
