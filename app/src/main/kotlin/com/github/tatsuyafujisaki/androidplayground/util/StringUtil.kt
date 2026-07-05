b0e8114d7c6b37151fa9b5c37bc5c99139bc525b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-07-06 00:45:00 +0900

    Remove getColoredSpannableString and unused imports

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/StringUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/StringUtil.kt
@@ -1,11 +1,7 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
 import android.text.Html
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
 import android.util.Patterns
-import androidx.annotation.ColorInt
 import androidx.core.text.htmlEncode
 
 @Suppress("unused")
@@ -19,18 +15,6 @@ object StringUtil {
      * Decodes HTML entities. For encoding HTML entities, use [htmlEncode].
      */
     fun decode(source: String) = Html.fromHtml(source, Html.FROM_HTML_MODE_COMPACT).toString()
-
-    fun getColoredSpannableString(
-        source: String,
-        @ColorInt color: Int,
-    ) = SpannableString(source).apply {
-        setSpan(
-            ForegroundColorSpan(color),
-            0,
-            length,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
-        )
-    }
 
     /**
      * https://developer.android.com/develop/ui/compose/navigation#optional-args
