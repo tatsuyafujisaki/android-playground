e2e6b1cdb61fab1a02a216faed39ed0e34c672bf
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-07-09 11:18:34 +0900

    Remove decode function and unused imports

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/StringUtil.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/util/StringUtil.kt
@@ -1,8 +1,6 @@
 package com.github.tatsuyafujisaki.androidplayground.util
 
-import android.text.Html
 import android.util.Patterns
-import androidx.core.text.htmlEncode
 
 @Suppress("unused")
 object StringUtil {
@@ -10,11 +8,6 @@ object StringUtil {
         Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()
 
     fun isValidPhoneNumber(phoneNumber: String) = Patterns.PHONE.matcher(phoneNumber).matches()
-
-    /**
-     * Decodes HTML entities. For encoding HTML entities, use [htmlEncode].
-     */
-    fun decode(source: String) = Html.fromHtml(source, Html.FROM_HTML_MODE_COMPACT).toString()
 
     /**
      * https://developer.android.com/develop/ui/compose/navigation#optional-args
