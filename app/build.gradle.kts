3bc5fac143f6cd5393006e5b471ac81e8d47ad12
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-08-03 07:56:18 +0900

    Remove coordinatorlayout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,7 +103,6 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
     implementation(libs.constraintlayout)
-    implementation(libs.coordinatorlayout)
     implementation(libs.datastore)
     implementation(libs.datastore.preferences)
     implementation(libs.firebase.analytics)
