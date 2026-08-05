cc785b7b3429bc7a8d99e8dce5cebb0bfdf73afe
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-08-05 09:58:20 +0900

    Remove LiveData usage and dependency

--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/viewmodel/MainViewModel.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/viewmodel/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,15 +9,8 @@ class MainViewModel : ViewModel() {
     private val _stateFlow = MutableStateFlow("")
     val stateFlow: StateFlow<String> = _stateFlow.asStateFlow()
 
-    private val _myLiveData = MutableLiveData("")
-    val myLiveData: LiveData<String> = _myLiveData
-
     fun setMyStateFlow(something: String) {
         _stateFlow.value = something
-    }
-
-    fun setMyLiveData(something: String) {
-        _myLiveData.value = something
     }
 
     companion object {
--- app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/viewmodel/MyActivityViewModel.kt
+++ app/src/main/kotlin/com/github/tatsuyafujisaki/androidplayground/ui/viewmodel/MyActivityViewModel.kt
@@ -1,17 +1,8 @@
 package com.github.tatsuyafujisaki.androidplayground.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MyActivityViewModel @Inject constructor() : ViewModel() {
-    private val _myLiveData = MutableLiveData("")
-    val myLiveData: LiveData<String> = _myLiveData
-
-    fun setMyLiveData(something: String) {
-        _myLiveData.value = something
-    }
-}
+class MyActivityViewModel @Inject constructor() : ViewModel()
